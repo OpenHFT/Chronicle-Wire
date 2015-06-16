921a64dcce05e086a94a11b1050be7266c08e708
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-06-16 19:53:34 +0100

    CE-57 fixed most of the remove test cases

--- src/main/java/net/openhft/chronicle/wire/YamlLogging.java
+++ src/main/java/net/openhft/chronicle/wire/YamlLogging.java
@@ -1,21 +1,16 @@
 package net.openhft.chronicle.wire;
 
-import org.jetbrains.annotations.NotNull;
-
 /**
  * Created by Rob Austin
  */
 public class YamlLogging {
 
-    public static final boolean showServerWrites = false;
+    public static volatile boolean showServerWrites = false;
     public static volatile boolean clientWrites = false;
     public static volatile String title = "";
-    @NotNull
     public static volatile String writeMessage = "";
     public static volatile boolean clientReads = false;
     public static volatile boolean showServerReads = false;
-
-    @NotNull
     public static String readMessage = "";
 }
 
