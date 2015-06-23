ec7e2c8868e9c4e44925974a27afa92ee14e7283
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-06-23 12:56:06 +0100

    Add serializable Function and updater interface and a test which serializes an Enum which is a function.

--- src/test/java/net/openhft/chronicle/wire/WireSerializedLambdaTest.java
+++ src/test/java/net/openhft/chronicle/wire/WireSerializedLambdaTest.java
@@ -17,6 +17,8 @@
 package net.openhft.chronicle.wire;
 
 import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.core.pool.ClassAliasPool;
+import net.openhft.chronicle.core.util.SerializableFunction;
 import org.junit.Test;
 
 import java.io.Serializable;
@@ -28,12 +30,15 @@ import static org.junit.Assert.*;
  * Created by peter on 23/06/15.
  */
 public class WireSerializedLambdaTest {
+    static {
+        ClassAliasPool.CLASS_ALIASES.addAlias(Fun.class);
+    }
     @Test
     public void testIsLambda() {
         Function<String, String> fun = (Function<String, String> & Serializable) String::toUpperCase;
         assertTrue(WireSerializedLambda.isSerializableLambda(fun.getClass()));
         int a = 5;
-        Function<Integer, Integer> fun2 = (Function<Integer, Integer> & Serializable) i -> i + a;
+        SerializableFunction<Integer, Integer> fun2 = i -> i + a;
         assertTrue(WireSerializedLambda.isSerializableLambda(fun2.getClass()));
         assertFalse(WireSerializedLambda.isSerializableLambda(this.getClass()));
     }
@@ -41,12 +46,14 @@ public class WireSerializedLambdaTest {
     @Test
     public void testTextWire() {
         Wire wire = new TextWire(Bytes.elasticByteBuffer());
-        Function<String, String> fun = (Function<String, String> & Serializable) String::toUpperCase;
-        wire.write().object(fun);
+        SerializableFunction<String, String> fun = String::toUpperCase;
+        wire.write(() -> "one").object(fun)
+                .write(() -> "two").object(Fun.ADD_A);
 
-        assertEquals("\"\": !SerializedLambda {\n" +
+
+        assertEquals("one: !SerializedLambda {\n" +
                 "  cc: !type net.openhft.chronicle.wire.WireSerializedLambdaTest,\n" +
-                "  fic: java/util/function/Function,\n" +
+                "  fic: net/openhft/chronicle/core/util/SerializableFunction,\n" +
                 "  fimn: apply,\n" +
                 "  fims: (Ljava/lang/Object;)Ljava/lang/Object;,\n" +
                 "  imk: 5,\n" +
@@ -55,39 +62,41 @@ public class WireSerializedLambdaTest {
                 "  ims: ()Ljava/lang/String;,\n" +
                 "  imt: (Ljava/lang/String;)Ljava/lang/String;,\n" +
                 "  ca: [  ]\n" +
-                "}\n", wire.bytes().toString());
+                "}\n" +
+                "two: !Fun ADD_A\n", wire.bytes().toString());
 
-        Function function = wire.read().object(Function.class);
+        Function function = wire.read(() -> "one").object(Function.class);
         assertEquals("HELLO", function.apply("hello"));
+
+        Function function2 = wire.read(() -> "two").object(Function.class);
+        assertEquals("helloA", function2.apply("hello"));
     }
 
     @Test
     public void testBinaryWire() {
         Wire wire = new BinaryWire(Bytes.elasticByteBuffer());
-        Function<String, String> fun = (Function<String, String> & Serializable) String::toUpperCase;
-        wire.write().object(fun);
+        SerializableFunction<String, String> fun = String::toUpperCase;
+        wire.write(() -> "one").object(fun)
+                .write(() -> "two").object(Fun.ADD_A);
 
-        assertEquals("00000000 C0 B6 10 53 65 72 69 61  6C 69 7A 65 64 4C 61 6D ···Seria lizedLam\n" +
-                "00000010 62 64 61 82 06 01 00 00  C2 63 63 BC 33 6E 65 74 bda····· ·cc·3net\n" +
-                "00000020 2E 6F 70 65 6E 68 66 74  2E 63 68 72 6F 6E 69 63 .openhft .chronic\n" +
-                "00000030 6C 65 2E 77 69 72 65 2E  57 69 72 65 53 65 72 69 le.wire. WireSeri\n" +
-                "00000040 61 6C 69 7A 65 64 4C 61  6D 62 64 61 54 65 73 74 alizedLa mbdaTest\n" +
-                "00000050 C3 66 69 63 FB 6A 61 76  61 2F 75 74 69 6C 2F 66 ·fic·jav a/util/f\n" +
-                "00000060 75 6E 63 74 69 6F 6E 2F  46 75 6E 63 74 69 6F 6E unction/ Function\n" +
-                "00000070 C4 66 69 6D 6E E5 61 70  70 6C 79 C4 66 69 6D 73 ·fimn·ap ply·fims\n" +
-                "00000080 B8 26 28 4C 6A 61 76 61  2F 6C 61 6E 67 2F 4F 62 ·&(Ljava /lang/Ob\n" +
-                "00000090 6A 65 63 74 3B 29 4C 6A  61 76 61 2F 6C 61 6E 67 ject;)Lj ava/lang\n" +
-                "000000a0 2F 4F 62 6A 65 63 74 3B  C3 69 6D 6B 05 C2 69 63 /Object; ·imk··ic\n" +
-                "000000b0 F0 6A 61 76 61 2F 6C 61  6E 67 2F 53 74 72 69 6E ·java/la ng/Strin\n" +
-                "000000c0 67 C3 69 6D 6E EB 74 6F  55 70 70 65 72 43 61 73 g·imn·to UpperCas\n" +
-                "000000d0 65 C3 69 6D 73 F4 28 29  4C 6A 61 76 61 2F 6C 61 e·ims·() Ljava/la\n" +
-                "000000e0 6E 67 2F 53 74 72 69 6E  67 3B C3 69 6D 74 B8 26 ng/Strin g;·imt·&\n" +
-                "000000f0 28 4C 6A 61 76 61 2F 6C  61 6E 67 2F 53 74 72 69 (Ljava/l ang/Stri\n" +
-                "00000100 6E 67 3B 29 4C 6A 61 76  61 2F 6C 61 6E 67 2F 53 ng;)Ljav a/lang/S\n" +
-                "00000110 74 72 69 6E 67 3B C2 63  61 82 00 00 00 00       tring;·c a·····  \n", wire.bytes().toHexString());
+        assertEquals("[pos: 0, rlim: 330, wlim: 8EiB, cap: 8EiB ] " +
+                "Ãone¶⒗SerializedLambda\\u0082 ⒈٠٠" +
+                "Âcc¼3net.openhft.chronicle.wire.WireSerializedLambdaTest" +
+                "Ãfic¸4net/openhft/chronicle/core/util/SerializableFunction" +
+                "Äfimnåapply" +
+                "Äfims¸&(Ljava/lang/Object;)Ljava/lang/Object;" +
+                "Ãimk⒌" +
+                "Âicðjava/lang/String" +
+                "ÃimnëtoUpperCase" +
+                "Ãimsô()Ljava/lang/String;" +
+                "Ãimt¸&(Ljava/lang/String;)Ljava/lang/String;" +
+                "Âca\\u0082٠٠٠٠" +
+                "Ãtwo¶⒊FunåADD_A", wire.bytes().toDebugString());
 
         Function function = wire.read().object(Function.class);
         assertEquals("HELLO", function.apply("hello"));
-    }
 
+        Function function2 = wire.read(() -> "two").object(Function.class);
+        assertEquals("helloA", function2.apply("hello"));
+    }
 }
