98ecd79faa0062248971bf238bd928889f9f2f6c
franz1981 <nigro.fra@gmail.com>
2015-07-12 12:25:35 +0200

    FIxed javadocs

--- src/main/java/net/openhft/chronicle/wire/util/BooleanConsumer.java
+++ src/main/java/net/openhft/chronicle/wire/util/BooleanConsumer.java
@@ -24,9 +24,9 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
 /**
- * Represents an operation that accepts a single {@code int}-valued argument and returns no result.  This is the
- * primitive type specialization of {@link java.util.function.Consumer} for {@code int}.  Unlike most other functional
- * interfaces, {@code IntConsumer} is expected to operate via side-effects.
+ * Represents an operation that accepts a single {@code boolean}-valued argument and returns no result.  This is the
+ * primitive type specialization of {@link java.util.function.Consumer} for {@code boolean}.  Unlike most other functional
+ * interfaces, {@code BooleanConsumer} is expected to operate via side-effects.
  *
  * <p>This is a <a href="package-summary.html">functional interface</a> whose functional method is {@link
  * #accept(Boolean)}.
@@ -44,12 +44,12 @@ public interface BooleanConsumer {
     void accept(Boolean value);
 
     /**
-     * Returns a composed {@code IntConsumer} that performs, in sequence, this operation followed by the {@code after}
+     * Returns a composed {@code BooleanConsumer} that performs, in sequence, this operation followed by the {@code after}
      * operation. If performing either operation throws an exception, it is relayed to the caller of the composed
      * operation.  If performing this operation throws an exception, the {@code after} operation will not be performed.
      *
      * @param after the operation to perform after this operation
-     * @return a composed {@code IntConsumer} that performs in sequence this operation followed by the {@code after}
+     * @return a composed {@code BooleanConsumer} that performs in sequence this operation followed by the {@code after}
      * operation
      * @throws NullPointerException if {@code after} is null
      */
