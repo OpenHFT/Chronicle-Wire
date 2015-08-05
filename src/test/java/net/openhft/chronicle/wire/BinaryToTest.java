26d2767f02cdc32b85ef6130e91fc2a3e99988fb
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-08-05 11:57:15 +0100

    added - as broken test

---
+++ src/test/java/net/openhft/chronicle/wire/BinaryToTest.java
@@ -0,0 +1,21 @@
+package net.openhft.chronicle.wire;
+
+import net.openhft.chronicle.bytes.Bytes;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Rob Austin.
+ */
+public class BinaryToTest {
+
+    @Ignore("JIRA https://higherfrequencytrading.atlassian.net/browse/WIRE-30")
+    @Test
+    public void test() throws Exception {
+        Bytes tbytes = Bytes.elasticByteBuffer();
+        Wire tw = new BinaryWire(tbytes);
+        tw.write(() -> "key").text("hello");
+        System.out.println(Wires.fromSizePrefixedBinaryToText(tbytes));
+    }
+
+}
