dfe6c8742390dcb24c1ded0408240b2da1b7ca64
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-10-27 15:39:23 +0000

    added a test for BinaryLongReferences

--- src/test/java/net/openhft/chronicle/wire/BinaryLongReferenceTest.java
+++ src/test/java/net/openhft/chronicle/wire/BinaryLongReferenceTest.java
@@ -15,11 +15,152 @@
  */
 package net.openhft.chronicle.wire;
 
-import junit.framework.TestCase;
+import net.openhft.chronicle.bytes.NativeBytesStore;
+import org.junit.Test;
 
-public class BinaryLongReferenceTest extends TestCase {
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
-    public void testBytes() {
+import static net.openhft.chronicle.bytes.NativeBytesStore.nativeStoreWithFixedCapacity;
+
+public class BinaryLongReferenceTest {
+
+    @Test
+    public void testPingPongUsingAtomics() throws InterruptedException {
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        AtomicLong inBound = new AtomicLong(1);
+        AtomicLong outBound = new AtomicLong(1);
+
+        executorService.submit(() -> {
+
+            try {
+
+                for (; ; ) {
+                    long value;
+                    do {
+                        value = inBound.get();
+                    } while (!inBound.compareAndSet(value, 0));
+
+                    for (; ; ) {
+                        if (outBound.compareAndSet(0, value))
+                            break;
+                        try {
+                            Thread.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            } catch (Throwable ex) {
+                ex.printStackTrace();
+            }
+        });
+        CountDownLatch latch = new CountDownLatch(1);
+        executorService.submit(() -> {
+
+            long lastValue = 0;
+
+            int i = 0;
+            long value = 0;
+            for (; value < 1000; ) {
+                if (inBound.compareAndSet(0, i)) {
+
+
+                    do {
+                        value = outBound.get();
+                    } while (!outBound.compareAndSet(value, 0));
+
+
+                    if (value != lastValue + 1 && value > 2)
+                        throw new AssertionError("");
+                    System.out.println("value=" + value);
+                    lastValue = value;
+                    i++;
+
+                }
+            }
+            latch.countDown();
+        });
+
+        latch.await();
+
+    }
+
+
+    @Test
+    public void testPointPongUsingBinaryLongReference() throws InterruptedException {
+
+        NativeBytesStore<Void> inBoundBytesStore = nativeStoreWithFixedCapacity(8);
+
+        BinaryLongReference inBound = new BinaryLongReference();
+        inBound.bytesStore(inBoundBytesStore, 0, 8);
+        inBound.setValue(1);
+
+        NativeBytesStore<Void> outBoundBytesStore = nativeStoreWithFixedCapacity(8);
+
+        BinaryLongReference outBound = new BinaryLongReference();
+        outBound.bytesStore(outBoundBytesStore, 0, 8);
+        outBound.setValue(1);
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.submit(() -> {
+
+            try {
+
+                for (; ; ) {
+                    long value;
+                    do {
+                        value = inBound.getVolatileValue();
+                    } while (!inBound.compareAndSwapValue(value, 0));
+
+                    for (; ; ) {
+                        if (outBound.compareAndSwapValue(0, value))
+                            break;
+                        try {
+                            Thread.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            } catch (Throwable ex) {
+                ex.printStackTrace();
+            }
+        });
+        CountDownLatch latch = new CountDownLatch(1);
+        executorService.submit(() -> {
+
+            long lastValue = 0;
+
+            int i = 0;
+            long value = 0;
+            for (; value < 1000; ) {
+                if (inBound.compareAndSwapValue(0, i)) {
+
+                    do {
+                        value = outBound.getVolatileValue();
+                    } while (!outBound.compareAndSwapValue(value, 0));
+
+
+                    if (value != lastValue + 1 && value > 2)
+                        throw new AssertionError("");
+                    System.out.println("value=" + value);
+                    lastValue = value;
+                    i++;
+
+                }
+            }
+            latch.countDown();
+        });
+
+        latch.await();
 
     }
 }
