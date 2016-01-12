cbf98ad1de06672d15130a289421bc0c15834b47
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-01-12 18:39:42 +0000

    work in progress, there are still some issue's I have to fix.

--- src/main/java/net/openhft/chronicle/wire/TextWire.java
+++ src/main/java/net/openhft/chronicle/wire/TextWire.java
@@ -530,6 +530,7 @@ public class TextWire implements Wire, InternalWire {
             try {
                 AppendableUtil.readUTFAndAppend(bytes, sb, testers);
             } catch (IOException e) {
+                e.printStackTrace();
                 throw new IORuntimeException(e);
             }
         }
