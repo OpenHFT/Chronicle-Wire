656de0438ebe77303a8d58a5d506e11964d9dcac
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-04-21 11:45:46 +0100

    FIx hanlding of nested collections and deprecate duplicate Map writing/reading methods.

--- src/test/java/net/openhft/chronicle/wire/InnerMapTest.java
+++ src/test/java/net/openhft/chronicle/wire/InnerMapTest.java
@@ -14,13 +14,36 @@ import java.util.Map;
  */
 public class InnerMapTest {
 
-    static class MyMarshable extends AbstractMarshallable {
+    @Test
+    public void testMyInnnerMap() {
+        MyMarshable myMarshable = new MyMarshable().name("rob");
+        myMarshable.commission().put("hello", 123.4);
 
+        String asString = myMarshable.toString();
+        Assert.assertEquals("!net.openhft.chronicle.wire.InnerMapTest$MyMarshable {\n" +
+                "  name: rob,\n" +
+                "  commission: {\n" +
+                "    hello: 123.4\n" +
+                "  }\n" +
+                "}\n", asString);
 
+        Bytes b = Bytes.elasticByteBuffer();
+        Wire w = new BinaryWire(b);     // works with text fails with binary
+
+        try (DocumentContext dc = w.writingDocument(false)) {
+            dc.wire().write("marshable").typedMarshallable(myMarshable);
+        }
+
+        try (DocumentContext dc = w.readingDocument()) {
+            MyMarshable tm = dc.wire().read(() -> "marshable").typedMarshallable();
+            Assert.assertEquals(asString, tm.toString());
+        }
+
+    }
+
+    static class MyMarshable extends AbstractMarshallable implements Demarshallable {
         String name;
-
         Map<String, Double> commission;
-
 
         @UsedViaReflection
         public MyMarshable(@NotNull WireIn wire) {
@@ -43,35 +66,5 @@ public class InnerMapTest {
             this.name = name;
             return this;
         }
-    }
-
-
-    @Test
-    public void testMyInnnerMap() {
-        MyMarshable myMarshable = new MyMarshable().name("rob");
-        myMarshable.commission().put("hello", 123.4);
-
-        String asString = myMarshable.toString();
-        Assert.assertEquals("!net.openhft.chronicle.wire.InnerMapTest$MyMarshable {\n" +
-                "  name: rob,\n" +
-                "  commission: {\n" +
-                "    hello: 123.4\n" +
-                "  }\n" +
-                "}\n", asString);
-
-
-        Bytes b = Bytes.elasticByteBuffer();
-        Wire w = new BinaryWire(b);     // works with text fails with binary
-
-        try (DocumentContext dc = w.writingDocument(false)) {
-            dc.wire().write("marshable").typedMarshallable(myMarshable);
-        }
-
-
-        try (DocumentContext dc = w.readingDocument()) {
-            MyMarshable tm = dc.wire().read(() -> "marshable").typedMarshallable();
-            Assert.assertEquals(asString, tm.toString());
-        }
-
     }
 }
