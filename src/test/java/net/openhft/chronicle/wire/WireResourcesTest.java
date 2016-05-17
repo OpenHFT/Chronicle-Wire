749a8241f113d6e5c15d94234e2efdf10b51054a
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-05-17 13:55:23 +0100

    Extend test for multiple mapping for one file.

--- src/test/java/net/openhft/chronicle/wire/WireResourcesTest.java
+++ src/test/java/net/openhft/chronicle/wire/WireResourcesTest.java
@@ -15,13 +15,16 @@
  */
 package net.openhft.chronicle.wire;
 
-import java.io.File;
-import java.nio.file.Files;
-
 import net.openhft.chronicle.bytes.MappedBytes;
 import net.openhft.chronicle.bytes.MappedFile;
-import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.StreamCorruptedException;
+import java.nio.file.Files;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -29,6 +32,13 @@ import static org.junit.Assert.assertEquals;
  * @author lburgazzoli
  */
 public class WireResourcesTest {
+
+    private static void writeMessage(Wire wire) throws TimeoutException, EOFException, StreamCorruptedException {
+        long pos = wire.writeHeader(1, TimeUnit.MILLISECONDS);
+        wire.bytes().writeSkip(32000);
+        wire.bytes().writeLong(1L);
+        wire.updateHeader(pos, false);
+    }
 
     @Test
     public void testMappedBytesClose() throws Exception {
@@ -90,30 +100,60 @@ public class WireResourcesTest {
         assertEquals(0, wire.bytes().refCount());
     }
 
-    @Ignore("mh!!")
     @Test
     public void testMappedBytesWireRelease2() throws Exception {
         File tmp = Files.createTempFile("chronicle-", ".wire").toFile();
         tmp.deleteOnExit();
 
-        Wire wire = WireType.TEXT.apply(MappedBytes.mappedBytes(tmp, 64 * 1024));
+        MappedBytes t = MappedBytes.mappedBytes(tmp, 64 * 1024);
+        assertEquals(1, t.refCount());
+        assertEquals(1, t.mappedFile().refCount());
+        Wire wire = WireType.TEXT.apply(t);
+        assertEquals(1, t.refCount()); // not really using it yet
+        assertEquals(1, t.mappedFile().refCount());
 
         assert wire.startUse();
         wire.headerNumber(1);
+        assertEquals(1, t.refCount());
+        assertEquals(1, t.mappedFile().refCount()); // not really using it yet
+
         wire.writeFirstHeader();
-        wire.updateFirstHeader();
+        assertEquals(1, wire.bytes().refCount());
+        assertEquals(2, t.mappedFile().refCount()); // now there is a mapping used as well as use in MappedBytes
 
         assertEquals(1, wire.bytes().refCount());
-        assertEquals(2, mappedFile(wire).refCount()); // Shouldn't be 1 ?
+        assertEquals(2, mappedFile(wire).refCount());
 
+        wire.bytes().writeSkip(32000);
+        wire.updateFirstHeader();
         assert wire.endUse();
 
+        writeMessage(wire);
+
+        assertEquals(2, mappedFile(wire).refCount());
+
+        writeMessage(wire);
+
+        // new block
+        assertEquals(3, mappedFile(wire).refCount());
+
+        writeMessage(wire);
+
+        assertEquals(3, mappedFile(wire).refCount());
+
+        writeMessage(wire);
+
+        assertEquals(4, mappedFile(wire).refCount());
+
         wire.bytes().release();
+        // the MappedFile was created by MappedBytes
+        // so when it is fully released, the MappedFile is close()d
         assertEquals(0, wire.bytes().refCount());
-        assertEquals(0, mappedFile(wire).refCount()); // Shouldn't be 1 ?
+        assertEquals(0, t.refCount());
+        assertEquals(0, mappedFile(wire).refCount());
     }
 
     protected MappedFile mappedFile(Wire wire) {
-        return ((MappedBytes)wire.bytes()).mappedFile();
+        return ((MappedBytes) wire.bytes()).mappedFile();
     }
 }
