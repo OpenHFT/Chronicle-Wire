97c4327ef39c3b1e3255d3c94d6c2bfc1b210386
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2017-05-17 14:24:29 +0100

    Fix Bytes.fromFile to read from JARs.

--- src/test/java/net/openhft/chronicle/wire/WireTypeTest.java
+++ src/test/java/net/openhft/chronicle/wire/WireTypeTest.java
@@ -16,6 +16,7 @@
 
 package net.openhft.chronicle.wire;
 
+import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.BytesUtil;
 import net.openhft.chronicle.core.OS;
 import net.openhft.chronicle.core.pool.ClassAliasPool;
@@ -26,6 +27,7 @@ import org.junit.Test;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by peter.lawrey on 10/02/2016.
@@ -98,5 +100,12 @@ public class WireTypeTest {
 
             assertEquals(tm, o);
         }
+    }
+
+    @Test
+    public void fromFileInJar() throws IOException {
+        Bytes bytes = BytesUtil.readFile("/net/openhft/chronicle/core/onoes/Google.properties");
+        long n = bytes.indexOf(Bytes.from("Apache License"));
+        assertTrue(n > 0);
     }
 }
