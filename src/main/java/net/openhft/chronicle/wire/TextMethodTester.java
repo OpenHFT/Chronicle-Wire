64093fe7e8ae677d8d35f4cbff6eac460da0fcb5
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2017-05-22 18:27:44 +0100

    Add an option to take only the last result for a given key set regardless of order

--- src/main/java/net/openhft/chronicle/wire/TextMethodTester.java
+++ src/main/java/net/openhft/chronicle/wire/TextMethodTester.java
@@ -4,6 +4,12 @@ import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.BytesUtil;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.function.Function;
 
 /**
@@ -20,12 +26,22 @@ public class TextMethodTester<T> {
 
     private String expected;
     private String actual;
+    private String[] retainLast;
 
     public TextMethodTester(String input, Function<T, Object> componentFunction, Class<T> outputClass, String output) {
         this.input = input;
         this.outputClass = outputClass;
         this.output = output;
         this.componentFunction = componentFunction;
+    }
+
+    public String[] retainLast() {
+        return retainLast;
+    }
+
+    public TextMethodTester retainLast(String... retainLast) {
+        this.retainLast = retainLast;
+        return this;
     }
 
     public String setup() {
@@ -49,7 +65,8 @@ public class TextMethodTester<T> {
     public TextMethodTester run() throws IOException {
 
         Wire wire2 = new TextWire(Bytes.allocateElasticDirect()).useTextDocuments();
-        T writer = wire2.methodWriter(outputClass);
+        T writer0 = wire2.methodWriter(outputClass);
+        T writer = retainLast == null ? writer0 : cachedMethodWriter(writer0);
         Object component = componentFunction.apply(writer);
 
         if (setup != null) {
@@ -65,10 +82,45 @@ public class TextMethodTester<T> {
         Wire wire = new TextWire(BytesUtil.readFile(input)).useTextDocuments();
 
         // expected
-        expected = BytesUtil.readFile(output).toString().trim().replace("\r", "");
+        if (retainLast == null) {
+            expected = BytesUtil.readFile(output).toString().trim().replace("\r", "");
+        } else {
+            Wire wireOut = new TextWire(BytesUtil.readFile(output));
+            Map<String, String> events = new TreeMap<>();
+            consumeDocumentSeperator(wireOut);
+            while (wireOut.hasMore()) {
+                StringBuilder event = new StringBuilder();
+                long start = wireOut.bytes().readPosition();
+                Map<String, Object> m = wireOut.read(event).marshallableAsMap(String.class, Object.class);
+                StringBuilder key = new StringBuilder(event);
+                for (String s : retainLast) {
+                    key.append(",").append(m.get(s));
+                }
+                long end = wireOut.bytes().readPosition();
+                events.put(key.toString(), wireOut.bytes().subBytes(start, end - start).toString().trim());
+                consumeDocumentSeperator(wireOut);
+            }
+            StringBuilder expected2 = new StringBuilder();
+            for (String s : events.values()) {
+                expected2.append(s.replace("\r", "")).append("\n");
+            }
+            expected = expected2.toString().trim();
+        }
         MethodReader reader = wire.methodReader(component);
+        long pos = wire2.bytes().writePosition();
         while (reader.readOne()) {
-            wire2.bytes().append("---\n");
+            if (retainLast == null || pos != wire2.bytes().writePosition())
+                wire2.bytes().append("---\n");
+            pos = wire2.bytes().writePosition();
+        }
+        if (retainLast != null) {
+            CachedInvocationHandler invocationHandler =
+                    (CachedInvocationHandler) Proxy.getInvocationHandler(writer);
+            try {
+                invocationHandler.flush();
+            } catch (Exception e) {
+                throw new IOException(e);
+            }
         }
         actual = wire2.toString().trim();
         if (afterRun != null) {
@@ -78,11 +130,68 @@ public class TextMethodTester<T> {
         return this;
     }
 
+    private void consumeDocumentSeperator(Wire wireOut) {
+        if (wireOut.bytes().peekUnsignedByte() == '-') {
+            wireOut.bytes().readSkip(3);
+        }
+    }
+
+    private T cachedMethodWriter(T writer0) {
+        Class[] interfaces = {outputClass};
+        return (T) Proxy.newProxyInstance(outputClass.getClassLoader(), interfaces, new CachedInvocationHandler(writer0));
+    }
+
     public String expected() {
         return expected;
     }
 
     public String actual() {
         return actual;
+    }
+
+    static class Invocation {
+        Method method;
+        Object[] args;
+
+        public Invocation(Method method, Object[] args) {
+            this.method = method;
+            this.args = args;
+        }
+    }
+
+    class CachedInvocationHandler implements InvocationHandler {
+        private final Map<String, Invocation> cache = new TreeMap<>();
+        private final T writer0;
+
+        public CachedInvocationHandler(T writer0) {
+            this.writer0 = writer0;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (method.getDeclaringClass() == Object.class) {
+                return method.invoke(this, args);
+            }
+
+            if (args != null && args.length == 1 && args[0] instanceof Marshallable) {
+                StringBuilder key = new StringBuilder();
+                key.append(method.getName());
+                Marshallable m = (Marshallable) args[0];
+                for (String s : retainLast) {
+                    key.append(",").append(m.getField(s, Object.class));
+                }
+                args[0] = m.deepCopy();
+                cache.put(key.toString(), new Invocation(method, args));
+            } else {
+                method.invoke(writer0, args);
+            }
+            return null;
+        }
+
+        public void flush() throws InvocationTargetException, IllegalAccessException {
+            for (Invocation invocation : cache.values()) {
+                invocation.method.invoke(writer0, invocation.args);
+            }
+        }
     }
 }
