c8028a574239210cdc8b285f438b9e605bd8cc90
rob <rob.austin@boundedbuffer.com>
2017-06-14 20:25:07 +0100

    added test

---
+++ src/test/java/net/openhft/chronicle/wire/WiresTest.java
@@ -0,0 +1,36 @@
+package net.openhft.chronicle.wire;
+
+import net.openhft.chronicle.bytes.Bytes;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by rob on 14/06/2017.
+ */
+public class WiresTest {
+
+
+    public static class Base {
+        Bytes base = Bytes.elasticByteBuffer();
+    }
+
+    @Test
+    public void test() {
+
+        Base base1 = new Base();
+        base1.base.clear().append("value1");
+
+        Base base2 = new Base();
+        base2.base.clear().append("value2");
+
+        Wires.reset(base1);
+        Wires.reset(base2);
+
+        base1.base.clear().append("value1");
+
+        Assert.assertEquals("value2", base2.base.toString());
+
+    }
+
+
+}
