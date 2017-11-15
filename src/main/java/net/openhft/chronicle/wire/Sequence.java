4c417be652443488ee1e327fb71476b5361b3a57
rob <rob.austin@boundedbuffer.com>
2017-11-15 22:57:03 +0000

    #402 added net.openhft.chronicle.queue.impl.single.SequenceImpl.SequenceImpl

--- src/main/java/net/openhft/chronicle/wire/Sequence.java
+++ src/main/java/net/openhft/chronicle/wire/Sequence.java
@@ -3,9 +3,22 @@ package net.openhft.chronicle.wire;
 public interface Sequence {
 
     /**
-     * returns the sequence or Long.MIN_VALUE if the sequence can not be resolved
+     * gets the sequence for a writePosition
+     * <p>
+     * This method will only return a valid sequence number of the write position if the write position is the
+     * last write position in the queue. YOU CAN NOT USE THIS METHOD TO LOOK UP RANDOM SEQUENCES FOR ANY WRITE POSITION.
+     * Long.MIN_VALUE will be return if a sequence number can not be found
+     *
+     * @param writePosition the last write position, expected to be the end of queue
+     * @return Long.MIN_VALUE if the sequence for this write position can not be found
      */
-    long sequence();
+    long sequence(long writePosition);
 
+    /**
+     * sets the sequence number for a writePosition
+     *
+     * @param sequence the sequence number
+     * @param position the write position
+     */
     void sequence(long sequence, long position);
 }
