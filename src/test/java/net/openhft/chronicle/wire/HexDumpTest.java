52055b68bccbdf724d6b2c47053df25c304589ac
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2018-01-18 13:04:18 +0000

    Add a test showing HexDumpBytes is little endian (on machines which are little endian)

--- src/test/java/net/openhft/chronicle/wire/HexDumpTest.java
+++ src/test/java/net/openhft/chronicle/wire/HexDumpTest.java
@@ -4,11 +4,16 @@ import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.HexDumpBytes;
 import org.junit.Test;
 
+import java.nio.ByteOrder;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class HexDumpTest {
     @Test
     public void testEndian() {
+        if (ByteOrder.nativeOrder() != ByteOrder.LITTLE_ENDIAN)
+            return;
+
         Bytes b = new HexDumpBytes();
         b.writeInt(0x0a0b0c0d);
         assertEquals("0d 0c 0b 0a\n", b.toHexString());
