fb233df2a58e1aac06a9e11ba4af237f72051f0d
Tom Shercliff <tomshercliff@gmail.com>
2018-07-02 10:30:05 +0100

    re-add testLongString

--- src/test/java/net/openhft/chronicle/wire/BinaryWireTest.java
+++ src/test/java/net/openhft/chronicle/wire/BinaryWireTest.java
@@ -735,6 +735,22 @@ public class BinaryWireTest {
         assertEquals(null, c);
     }
 
+    @Test
+    public void testLongString() {
+        @NotNull Wire wire = createWire();
+        @NotNull char[] chars = new char[128];
+        for (int i = 0; i < Character.MAX_VALUE; i++) {
+            if (!Character.isValidCodePoint(i))
+                continue;
+            wire.clear();
+            Arrays.fill(chars, (char) i);
+            @NotNull String s = new String(chars);
+            wire.writeDocument(false, w -> w.write(() -> "message").text(s));
+
+//            System.out.println(Wires.fromSizePrefixedBlobs(wire.bytes()));
+            wire.readDocument(null, w -> w.read(() -> "message").text(s, Assert::assertEquals));
+        }
+    }
 
     @Test
     public void testArrays() {
