7cb4d31aa7e6ee24e49e29aa5289b187710b81aa
Rob Austin <rob.austin@boundedbuffer.co.uk>
2018-07-24 14:14:39 +0100

    fixed bitset test https://github.com/OpenHFT/Chronicle-Wire/issues/82

--- src/test/java/net/openhft/chronicle/wire/BitSetTest.java
+++ src/test/java/net/openhft/chronicle/wire/BitSetTest.java
@@ -14,64 +14,78 @@ public class BitSetTest {
     @Test
     public void testBitSetEquals() {
         Bytes b = Bytes.elasticByteBuffer();
-        Wire wire = WireType.TEXT.apply(b);
+        try {
+            Wire wire = WireType.TEXT.apply(b);
 
-        BitSet original = new BitSet(64);
-        original.set(10);
-        wire.getValueOut().object(original);
+            BitSet original = new BitSet(64);
+            original.set(10);
+            wire.getValueOut().object(original);
 
-        BitSet read = wire.getValueIn().object(BitSet.class);
-        Assert.assertEquals(original, read);
-
+            BitSet read = wire.getValueIn().object(BitSet.class);
+            Assert.assertEquals(original, read);
+        } finally {
+            b.release();
+        }
     }
 
     @Test
     public void testBitSetEquals2() {
         Bytes b = Bytes.elasticByteBuffer();
-        Wire wire = WireType.TEXT.apply(b);
+        try {
+            Wire wire = WireType.TEXT.apply(b);
 
-        BitSet original = new BitSet(64);
-        original.set(10);
-        original.set(89);
-        wire.getValueOut().object(original);
+            BitSet original = new BitSet(64);
+            original.set(10);
+            original.set(89);
+            wire.getValueOut().object(original);
 
-        BitSet read = wire.getValueIn().object(BitSet.class);
-        Assert.assertEquals(original, read);
-
+            BitSet read = wire.getValueIn().object(BitSet.class);
+            Assert.assertEquals(original, read);
+        } finally {
+            b.release();
+        }
     }
 
     @Test
     public void testBitSetToText() {
         Bytes b = Bytes.elasticByteBuffer();
-        Wire wire = WireType.TEXT.apply(b);
+        try {
+            Wire wire = WireType.TEXT.apply(b);
 
-        BitSet bs = new BitSet(64);
-        bs.set(10);
+            BitSet bs = new BitSet(64);
+            bs.set(10);
 
-        wire.getValueOut().object(bs);
-        Assert.assertEquals("!!bitset [\n" +
-                "  1024,\n" +
-                "  # 0000000000000000000000000000000000000000000000000000010000000000\n" +
-                "\n" +
-                "]", wire.toString());
+            wire.getValueOut().object(bs);
+            Assert.assertEquals("!!bitset [\n" +
+                    "  1024,\n" +
+                    "  # 0000000000000000000000000000000000000000000000000000010000000000\n" +
+                    "\n" +
+                    "]", wire.toString());
+        } finally {
+            b.release();
+        }
     }
 
     @Test
     public void testBitSet2ToText() {
         Bytes b = Bytes.elasticByteBuffer();
-        Wire wire = WireType.TEXT.apply(b);
+        try {
+            Wire wire = WireType.TEXT.apply(b);
 
-        BitSet bs = new BitSet(64);
-        bs.set(10);
-        bs.set(89);
-        wire.getValueOut().object(bs);
-        Assert.assertEquals("!!bitset [\n" +
-                "  1024,\n" +
-                "  # 0000000000000000000000000000000000000000000000000000010000000000\n" +
-                "  33554432,\n" +
-                "  # 0000000000000000000000000000000000000010000000000000000000000000\n" +
-                "\n" +
-                "]", wire.toString());
+            BitSet bs = new BitSet(64);
+            bs.set(10);
+            bs.set(89);
+            wire.getValueOut().object(bs);
+            Assert.assertEquals("!!bitset [\n" +
+                    "  1024,\n" +
+                    "  # 0000000000000000000000000000000000000000000000000000010000000000\n" +
+                    "  33554432,\n" +
+                    "  # 0000000000000000000000000000000000000010000000000000000000000000\n" +
+                    "\n" +
+                    "]", wire.toString());
+        } finally {
+            b.release();
+        }
     }
 
     @Test
@@ -81,15 +95,18 @@ public class BitSetTest {
         using.set(1);
 
         Bytes b = Bytes.elasticByteBuffer();
-        Wire wire = WireType.TEXT.apply(b);
+        try {
+            Wire wire = WireType.TEXT.apply(b);
 
-        BitSet original = new BitSet(64);
-        original.set(10);
-        original.set(89);
-        wire.getValueOut().object(original);
+            BitSet original = new BitSet(64);
+            original.set(10);
+            original.set(89);
+            wire.getValueOut().object(original);
 
-        BitSet read = wire.getValueIn().object(using, BitSet.class);
-        Assert.assertEquals(original, read);
-
+            BitSet read = wire.getValueIn().object(using, BitSet.class);
+            Assert.assertEquals(original, read);
+        } finally {
+            b.release();
+        }
     }
 }
