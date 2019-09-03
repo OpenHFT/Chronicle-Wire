05bd2d3af9a7832732d52e68a452aff9e7ac057a
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2019-09-03 09:01:03 +0100

    Fix up version dependencies.

--- src/test/java/net/openhft/chronicle/wire/MessagePathClassifierTest.java
+++ src/test/java/net/openhft/chronicle/wire/MessagePathClassifierTest.java
@@ -16,20 +16,20 @@ public class MessagePathClassifierTest {
 
     @Test
     public void pathFor() {
-        MessagePathClassifier mpc =
-                new MessagePathClassifier()
-                        .addPathForSourcesEnding(4, 4)
-                        .addPathForSourcesEnding(123, 1, 2, 3)
-                        .addPathForSourcesEnding(23, 2, 3)
-                        .addPathForSourcesEnding(43, 4, 3)
-                        .addPathForSourcesEnding(3, 3)
-                        .addPathForSourcesEnding(0);
-        assertEquals(3, mpc.pathFor(messageHistory("sources: [ 3, 0 ]")));
-        assertEquals(43, mpc.pathFor(messageHistory("sources: [ 4, 0, 3, 0 ]")));
-        assertEquals(4, mpc.pathFor(messageHistory("sources: [ 3, 0, 4, 0 ]")));
-        assertEquals(23, mpc.pathFor(messageHistory("sources: [ 2, 0, 3, 0 ]")));
-        assertEquals(123, mpc.pathFor(messageHistory("sources: [ 1, 0, 2, 0, 3, 0 ]")));
-        assertEquals(0, mpc.pathFor(messageHistory("sources: [ 1, 0, 2, 0, 5, 0 ]")));
+MessagePathClassifier mpc =
+        new MessagePathClassifier()
+                .addPathForSourcesEnding(4, 4)
+                .addPathForSourcesEnding(123, 1, 2, 3)
+                .addPathForSourcesEnding(23, 2, 3)
+                .addPathForSourcesEnding(43, 4, 3)
+                .addPathForSourcesEnding(3, 3)
+                .addPathForSourcesEnding(0);
+assertEquals(3, mpc.pathFor(messageHistory("sources: [ 3, 0 ]")));
+assertEquals(43, mpc.pathFor(messageHistory("sources: [ 4, 0, 3, 0 ]")));
+assertEquals(4, mpc.pathFor(messageHistory("sources: [ 3, 0, 4, 0 ]")));
+assertEquals(23, mpc.pathFor(messageHistory("sources: [ 2, 0, 3, 0 ]")));
+assertEquals(123, mpc.pathFor(messageHistory("sources: [ 1, 0, 2, 0, 3, 0 ]")));
+assertEquals(0, mpc.pathFor(messageHistory("sources: [ 1, 0, 2, 0, 5, 0 ]")));
     }
 
     @Test
