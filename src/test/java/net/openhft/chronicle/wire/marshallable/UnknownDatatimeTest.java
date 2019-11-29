b87b4231b11830a1a4c21a4fa4da2b294f8f5fc9
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2019-11-29 11:14:28 +0000

    Add a test for an unexpected DateTime field.

---
+++ src/test/java/net/openhft/chronicle/wire/marshallable/UnknownDatatimeTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2016-2019 Chronicle Software Ltd
+ */
+
+package net.openhft.chronicle.wire.marshallable;
+
+import net.openhft.chronicle.wire.Marshallable;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnknownDatatimeTest {
+    @Test
+    public void ignoreAnUnknownDateTime() throws IOException {
+        AClass aClass = Marshallable.fromString("!" + AClass.class.getName() + " { eventTime: 2019-04-02T11:20:41.616653, id: 123456 }");
+        assertEquals(123456, aClass.id);
+    }
+}
