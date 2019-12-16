16fc29f6424800096d9cd2c9aa082a06a814442c
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2019-12-16 18:43:06 +0000

    Added support for chained method reader/writers, https://github.com/OpenHFT/Chronicle-Wire/issues/152

--- src/main/java/net/openhft/chronicle/wire/VanillaMethodWriterBuilder.java
+++ src/main/java/net/openhft/chronicle/wire/VanillaMethodWriterBuilder.java
@@ -73,7 +73,7 @@ public class VanillaMethodWriterBuilder<T> implements Supplier<T>, MethodWriterB
         interfaces.add(additionalClass);
         for (Method method : additionalClass.getMethods()) {
             Class<?> returnType = method.getReturnType();
-            if (returnType.isInterface() && Jvm.dontChain(returnType)) {
+            if (returnType.isInterface() && !Jvm.dontChain(returnType)) {
                 addInterface(returnType);
             }
         }
