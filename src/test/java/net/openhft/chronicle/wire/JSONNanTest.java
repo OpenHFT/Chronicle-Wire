5e8b00369836cb393cad568c80071b4c59c49cfd
Rob Austin <rob.austin@boundedbuffer.co.uk>
2019-12-18 15:06:58 +0000

    added failing test for NAN

---
+++ src/test/java/net/openhft/chronicle/wire/JSONNanTest.java
@@ -0,0 +1,36 @@
+package net.openhft.chronicle.wire;
+
+import net.openhft.chronicle.bytes.Bytes;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JSONNanTest {
+
+    @Test
+    public void writeNaN() {
+        Bytes b = Bytes.elasticByteBuffer();
+
+        Wire wire = WireType.JSON.apply(b);
+        Dto value = new Dto();
+        value.value = Double.NaN;
+        wire.write().object(value);
+        Assert.assertEquals("\"\":{\"value\":null}", wire.toString());
+    }
+
+    /**
+     * JSON spec says that NAN should be written as null
+     */
+    @Test
+    public void readNan() {
+        Bytes b = Bytes.fromString("\"\":{\"value\":null}");
+        Wire wire = WireType.JSON.apply(b);
+        Dto value = wire.read().object(Dto.class);
+        Assert.assertEquals(Double.NaN, value.value);
+
+    }
+
+    public static class Dto extends SelfDescribingMarshallable {
+        double value;
+    }
+
+}
