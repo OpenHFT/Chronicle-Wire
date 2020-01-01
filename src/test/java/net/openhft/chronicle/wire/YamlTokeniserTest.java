b6bc067032bd07fd7950f26ed33ee87de42eae08
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-01-01 21:31:43 +0000

    First stage Yaml Parser.

--- src/test/java/net/openhft/chronicle/wire/YamlTokeniserTest.java
+++ src/test/java/net/openhft/chronicle/wire/YamlTokeniserTest.java
@@ -2,6 +2,7 @@ package net.openhft.chronicle.wire;
 
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.BytesUtil;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -375,6 +376,236 @@ public class YamlTokeniserTest {
                         "MAPPING_END \n" +
                         "DOCUMENT_END \n",
                 doTest("yaml/spec/2_17QuotedScalars.yaml"));
+    }
+
+    @Test
+    public void eg2_19() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT canonical\n" +
+                        "TEXT 12345\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT decimal\n" +
+                        "TEXT +12345\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT octal\n" +
+                        "TEXT 0o14\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT hexadecimal\n" +
+                        "TEXT 0xC\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_19Integers.yaml"));
+    }
+
+    @Test
+    public void eg2_20() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT canonical\n" +
+                        "TEXT 1.23015e+3\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT exponential\n" +
+                        "TEXT 12.3015e+02\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT fixed\n" +
+                        "TEXT 1230.15\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT negative infinity\n" +
+                        "TEXT -.inf\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT not a number\n" +
+                        "TEXT .NaN\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_20FloatingPoint.yaml"));
+    }
+
+    @Test
+    public void eg2_21() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT null\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT booleans\n" +
+                        "SEQUENCE_START \n" +
+                        "SEQUENCE_ENTRY \n" +
+                        "TEXT true\n" +
+                        "SEQUENCE_ENTRY \n" +
+                        "TEXT false\n" +
+                        "SEQUENCE_END \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT string\n" +
+                        "TEXT 012345\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_21Miscellaneous.yaml"));
+    }
+
+    @Test
+    public void eg2_22() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT canonical\n" +
+                        "TEXT 2001-12-15T02:59:43.1Z\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT iso8601\n" +
+                        "TEXT 2001-12-14t21:59:43.10-05:00\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT spaced\n" +
+                        "TEXT 2001-12-14 21:59:43.10 -5\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT date\n" +
+                        "TEXT 2002-12-14\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_22Timestamps.yaml"));
+    }
+
+    @Ignore("TODO FIX")
+    @Test
+    public void eg2_23() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT canonical\n" +
+                        "TEXT 2001-12-15T02:59:43.1Z\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT iso8601\n" +
+                        "TEXT 2001-12-14t21:59:43.10-05:00\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT spaced\n" +
+                        "TEXT 2001-12-14 21:59:43.10 -5\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT date\n" +
+                        "TEXT 2002-12-14\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_23VariousExplicitTags.yaml"));
+    }
+
+    @Ignore("TODO FIX")
+    @Test
+    public void eg2_24() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT canonical\n" +
+                        "TEXT 2001-12-15T02:59:43.1Z\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT iso8601\n" +
+                        "TEXT 2001-12-14t21:59:43.10-05:00\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT spaced\n" +
+                        "TEXT 2001-12-14 21:59:43.10 -5\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT date\n" +
+                        "TEXT 2002-12-14\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_24GlobalTags.yaml"));
+    }
+
+    @Ignore("TODO FIX")
+    @Test
+    public void eg2_25() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT canonical\n" +
+                        "TEXT 2001-12-15T02:59:43.1Z\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT iso8601\n" +
+                        "TEXT 2001-12-14t21:59:43.10-05:00\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT spaced\n" +
+                        "TEXT 2001-12-14 21:59:43.10 -5\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT date\n" +
+                        "TEXT 2002-12-14\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_25UnorderedSets.yaml"));
+    }
+
+    @Ignore("TODO FIX")
+    @Test
+    public void eg2_26() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT canonical\n" +
+                        "TEXT 2001-12-15T02:59:43.1Z\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT iso8601\n" +
+                        "TEXT 2001-12-14t21:59:43.10-05:00\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT spaced\n" +
+                        "TEXT 2001-12-14 21:59:43.10 -5\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT date\n" +
+                        "TEXT 2002-12-14\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_26OrderedMappings.yaml"));
+    }
+
+    @Ignore("TODO FIX")
+    @Test
+    public void eg2_27() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT canonical\n" +
+                        "TEXT 2001-12-15T02:59:43.1Z\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT iso8601\n" +
+                        "TEXT 2001-12-14t21:59:43.10-05:00\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT spaced\n" +
+                        "TEXT 2001-12-14 21:59:43.10 -5\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT date\n" +
+                        "TEXT 2002-12-14\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_27Invoice.yaml"));
+    }
+
+    @Ignore("TODO FIX")
+    @Test
+    public void eg2_28() {
+        assertEquals(
+                "DIRECTIVES_END \n" +
+                        "MAPPING_START \n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT canonical\n" +
+                        "TEXT 2001-12-15T02:59:43.1Z\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT iso8601\n" +
+                        "TEXT 2001-12-14t21:59:43.10-05:00\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT spaced\n" +
+                        "TEXT 2001-12-14 21:59:43.10 -5\n" +
+                        "MAPPING_KEY \n" +
+                        "TEXT date\n" +
+                        "TEXT 2002-12-14\n" +
+                        "MAPPING_END \n" +
+                        "DOCUMENT_END \n",
+                doTest("yaml/spec/2_28LogFile.yaml"));
     }
 
 
