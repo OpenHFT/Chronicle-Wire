8d5a8ee0a46a1b5b1a4a44873c3a2b7f1de426bb
Rob Austin <rob.austin@boundedbuffer.co.uk>
2020-01-31 11:51:00 +0000

    fixed test

--- src/test/java/net/openhft/chronicle/wire/JSONNanTest.java
+++ src/test/java/net/openhft/chronicle/wire/JSONNanTest.java
@@ -9,12 +9,15 @@ public class JSONNanTest {
     @Test
     public void writeNaN() {
         Bytes b = Bytes.elasticByteBuffer();
-
-        Wire wire = WireType.JSON.apply(b);
-        Dto value = new Dto();
-        value.value = Double.NaN;
-        wire.write().object(value);
-        Assert.assertEquals("\"\":{\"value\":null}", wire.toString());
+        try {
+            Wire wire = WireType.JSON.apply(b);
+            Dto value = new Dto();
+            value.value = Double.NaN;
+            wire.write().object(value);
+            Assert.assertEquals("\"\":{\"value\":null}", wire.toString());
+        } finally {
+            b.release();
+        }
     }
 
     /**
