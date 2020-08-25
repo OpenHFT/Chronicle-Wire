79a43fc70b839a3fe9f8efb5aa2a9acd0d4a9a1e
Jerry Shea <jerry.shea@chronicle.software>
2020-08-26 09:47:25 +1000

    ServicesTimestampLongConverter https://github.com/ChronicleEnterprise/Chronicle-Services/issues/177

---
+++ src/main/java/net/openhft/chronicle/wire/ServicesTimestampLongConverter.java
@@ -0,0 +1,68 @@
+package net.openhft.chronicle.wire;
+
+import net.openhft.chronicle.core.annotation.UsedViaReflection;
+import net.openhft.chronicle.core.time.LongTime;
+import net.openhft.chronicle.core.time.SystemTimeProvider;
+import net.openhft.chronicle.core.time.TimeProvider;
+import net.openhft.chronicle.core.time.UniqueMicroTimeProvider;
+
+import java.util.function.ToLongFunction;
+
+import static net.openhft.chronicle.core.time.SystemTimeProvider.CLOCK;
+
+public class ServicesTimestampLongConverter implements LongConverter {
+    @UsedViaReflection
+    public static final ServicesTimestampLongConverter INSTANCE = new ServicesTimestampLongConverter();
+
+    private static final String SERVICES_TIME_UNIT = System.getProperty("service.time.unit", "ns");
+    private static final longFunction toTime;
+    private static final ToLongFunction<TimeProvider> currentTime;
+    private static final LongConverter underlying;
+
+    static {
+        switch (SERVICES_TIME_UNIT) {
+            case "ms":
+                toTime = LongTime::toMillis;
+                currentTime = TimeProvider::currentTimeMillis;
+                underlying = MilliTimestampLongConverter.INSTANCE;
+                break;
+            case "us":
+                toTime = LongTime::toMicros;
+                currentTime = TimeProvider::currentTimeMicros;
+                underlying = MicroTimestampLongConverter.INSTANCE;
+                break;
+            case "ns":
+            default:
+                toTime = LongTime::toNanos;
+                currentTime = TimeProvider::currentTimeNanos;
+                underlying = NanoTimestampLongConverter.INSTANCE;
+                break;
+        }
+    }
+
+    public static long toTime(long arg) {
+        return toTime.apply(arg);
+    }
+
+    public static long currentTime() {
+        TimeProvider clock = CLOCK;
+        // if it is default use unique time provider instead.
+        if (clock == SystemTimeProvider.INSTANCE)
+            clock = UniqueMicroTimeProvider.INSTANCE;
+        return currentTime.applyAsLong(clock);
+    }
+
+    @Override
+    public long parse(CharSequence text) {
+        return underlying.parse(text);
+    }
+
+    @Override
+    public void append(StringBuilder text, long value) {
+        underlying.append(text, value);
+    }
+
+    interface longFunction {
+        long apply(long value);
+    }
+}
