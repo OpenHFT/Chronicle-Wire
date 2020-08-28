6185c884b57590c6daaf0b89891765d5160dbe10
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-08-28 08:06:10 +0100

    Make the handling of default methods the readers job, not the writer. https://github.com/OpenHFT/Chronicle-Wire/issues/192

---
+++ src/test/java/net/openhft/chronicle/wire/method/DefaultMethodHandlingTest.java
@@ -0,0 +1,30 @@
+package net.openhft.chronicle.wire.method;
+
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.wire.TextWire;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+interface WithDefault {
+    void method1(String text);
+
+    default void method2(String text2) {
+    }
+}
+
+public class DefaultMethodHandlingTest {
+    @Ignore("TODO FIX https://github.com/OpenHFT/Chronicle-Wire/issues/192")
+    @Test
+    public void withDefault() {
+        TextWire wire = new TextWire(Bytes.allocateElasticOnHeap());
+        WithDefault withDefault = wire.methodWriter(WithDefault.class);
+        withDefault.method1("one");
+        withDefault.method2("two");
+        assertEquals("method1: one\n" +
+                "...\n" +
+                "method2: two\n" +
+                "...\n", wire.toString());
+    }
+}
