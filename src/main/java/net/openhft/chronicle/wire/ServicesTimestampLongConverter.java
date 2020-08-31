3f529ea1cd8e9f62a697e476fc09224e4a7d758e
Jerry Shea <jerry.shea@chronicle.software>
2020-08-31 21:24:07 +1000

    ServicesTimestampLongConverter.timeUnit

--- src/main/java/net/openhft/chronicle/wire/ServicesTimestampLongConverter.java
+++ src/main/java/net/openhft/chronicle/wire/ServicesTimestampLongConverter.java
@@ -6,6 +6,7 @@ import net.openhft.chronicle.core.time.SystemTimeProvider;
 import net.openhft.chronicle.core.time.TimeProvider;
 import net.openhft.chronicle.core.time.UniqueMicroTimeProvider;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.ToLongFunction;
 
 import static net.openhft.chronicle.core.time.SystemTimeProvider.CLOCK;
@@ -18,6 +19,7 @@ public class ServicesTimestampLongConverter implements LongConverter {
     private static final longFunction toTime;
     private static final ToLongFunction<TimeProvider> currentTime;
     private static final LongConverter underlying;
+    private static final TimeUnit timeUnit;
 
     static {
         switch (SERVICES_TIME_UNIT) {
@@ -25,17 +27,20 @@ public class ServicesTimestampLongConverter implements LongConverter {
                 toTime = LongTime::toMillis;
                 currentTime = TimeProvider::currentTimeMillis;
                 underlying = MilliTimestampLongConverter.INSTANCE;
+                timeUnit = TimeUnit.MILLISECONDS;
                 break;
             case "us":
                 toTime = LongTime::toMicros;
                 currentTime = TimeProvider::currentTimeMicros;
                 underlying = MicroTimestampLongConverter.INSTANCE;
+                timeUnit = TimeUnit.MICROSECONDS;
                 break;
             case "ns":
             default:
                 toTime = LongTime::toNanos;
                 currentTime = TimeProvider::currentTimeNanos;
                 underlying = NanoTimestampLongConverter.INSTANCE;
+                timeUnit = TimeUnit.NANOSECONDS;
                 break;
         }
     }
@@ -54,6 +59,10 @@ public class ServicesTimestampLongConverter implements LongConverter {
 
     public static long currentTime(TimeProvider clock) {
         return currentTime.applyAsLong(clock);
+    }
+
+    public static TimeUnit timeUnit() {
+        return timeUnit;
     }
 
     @Override
