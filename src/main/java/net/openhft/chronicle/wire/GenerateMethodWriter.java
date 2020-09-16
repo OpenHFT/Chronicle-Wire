aa9aa1fdd8cc0c158dab9fa15910a031df8deaf7
Rob Austin <rob.austin@boundedbuffer.co.uk>
2020-09-16 11:02:20 +0100

    #455 un ignored dump code

--- src/main/java/net/openhft/chronicle/wire/GenerateMethodWriter.java
+++ src/main/java/net/openhft/chronicle/wire/GenerateMethodWriter.java
@@ -342,8 +342,8 @@ public class GenerateMethodWriter {
             imports.append(interfaceMethods);
             imports.append("\n}\n");
 
-            //   if (DUMP_CODE)
-            System.out.println(imports);
+            if (DUMP_CODE)
+                System.out.println(imports);
 
             return CACHED_COMPILER.loadFromJava(classLoader, packageName + '.' + className, imports.toString());
 
