4a352944e0a62619a45fbec8a29194c2286a1455
Jerry Shea <jerry.shea@chronicle.software>
2020-09-18 13:10:33 +1000

    check MethodReader default methods

--- src/test/java/net/openhft/chronicle/wire/method/DefaultMethodHandlingTest.java
+++ src/test/java/net/openhft/chronicle/wire/method/DefaultMethodHandlingTest.java
@@ -1,15 +1,20 @@
 package net.openhft.chronicle.wire.method;
 
 import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.bytes.MethodReader;
+import net.openhft.chronicle.core.Mocker;
 import net.openhft.chronicle.wire.TextWire;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.io.StringWriter;
+
+import static org.junit.Assert.*;
 
 interface WithDefault {
     void method1(String text);
 
     default void method2(String text2) {
+        throw new UnsupportedOperationException();
     }
 }
 
@@ -24,5 +29,13 @@ public class DefaultMethodHandlingTest {
                 "...\n" +
                 "method2: two\n" +
                 "...\n", wire.toString());
+
+        StringWriter sw = new StringWriter();
+        MethodReader reader = wire.methodReader(Mocker.logging(WithDefault.class, "", sw));
+        assertTrue(reader.readOne());
+        assertTrue(reader.readOne());
+        assertFalse(reader.readOne());
+        assertEquals("method1[one]\n" +
+                "method2[two]\n", sw.toString().replace("\r", ""));
     }
 }
