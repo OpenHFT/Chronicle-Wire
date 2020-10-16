c2f8ac6cafb344d7954f9be917b3bad39a71340d
eduard.shangareev <eduard.shangareev@bitorb.com>
2020-10-16 16:40:41 +0300

    Added reset issue reproduction

---
+++ src/test/java/net/openhft/chronicle/wire/reproducer/ResetWeirdBehaviour.java
@@ -0,0 +1,34 @@
+package net.openhft.chronicle.wire.reproducer;
+
+import net.openhft.chronicle.core.io.Closeable;
+import net.openhft.chronicle.wire.SelfDescribingMarshallable;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ResetWeirdBehaviour {
+    @Test
+    public void test() throws Exception {
+        Event event = new Event();
+        Assert.assertFalse(event.isClosed());
+
+        event.reset();
+        Assert.assertFalse(event.isClosed());
+    }
+
+
+    public static class Event extends SelfDescribingMarshallable implements Closeable {
+        private boolean isClosed;
+
+        //other fields
+
+        @Override
+        public void close() {
+            isClosed = true;
+        }
+
+        @Override
+        public boolean isClosed() {
+            return isClosed;
+        }
+    }
+}
