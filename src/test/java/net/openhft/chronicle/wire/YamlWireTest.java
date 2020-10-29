e7d111c293855224cfc0f73733242d425e8ba9e9
Jonathan Stein <jsstein3@illinois.edu>
2020-10-28 23:21:54 -0500

    Fix Flaky single quote tests in TestWireTest and YamlWireTest

--- src/test/java/net/openhft/chronicle/wire/YamlWireTest.java
+++ src/test/java/net/openhft/chronicle/wire/YamlWireTest.java
@@ -702,6 +702,7 @@ public class YamlWireTest extends WireTestCommon {
 
     @Test
     public void testSingleQuote() {
+        ClassAliasPool.CLASS_ALIASES.addAlias(YNestedA.class);
         YNestedA a = Marshallable.fromString("!YNestedA {\n" +
                 "  b: !YNestedB,\n" +
                 "  value: 12345\n" +
