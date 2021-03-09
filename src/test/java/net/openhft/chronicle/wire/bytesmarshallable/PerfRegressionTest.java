6dc2aea35f863e06b8721c598a5caf902b997783
Peter Lawrey <peter.lawrey@chronicle.software>
2021-03-09 14:24:28 +0000

    Added performant memory access operations to use in Bytes, https://github.com/OpenHFT/Chronicle-Core/issues/209

---
+++ src/test/java/net/openhft/chronicle/wire/bytesmarshallable/PerfRegressionTest.java
@@ -0,0 +1,96 @@
+package net.openhft.chronicle.wire.bytesmarshallable;
+
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.bytes.BytesIn;
+import net.openhft.chronicle.bytes.BytesOut;
+import net.openhft.chronicle.bytes.BytesStore;
+import net.openhft.chronicle.core.io.IORuntimeException;
+import net.openhft.chronicle.wire.BytesInBinaryMarshallable;
+import org.junit.Test;
+
+import java.nio.BufferOverflowException;
+import java.nio.BufferUnderflowException;
+
+public class PerfRegressionTest {
+
+    static class BytesFields extends BytesInBinaryMarshallable {
+        Bytes a = Bytes.allocateElasticOnHeap();
+        Bytes b = Bytes.allocateElasticOnHeap();
+        Bytes c = Bytes.allocateElasticOnHeap();
+        Bytes d = Bytes.allocateElasticOnHeap();
+
+        public BytesFields() {
+        }
+
+        public BytesFields(String a, String b, String c, String d) {
+            this();
+            this.a.append(a);
+            this.b.append(b);
+            this.c.append(c);
+            this.d.append(d);
+        }
+    }
+
+    static class DefaultBytesFields extends BytesFields {
+        public DefaultBytesFields() {
+        }
+
+        public DefaultBytesFields(String a, String b, String c, String d) {
+            super(a, b, c, d);
+        }
+
+        @Override
+        public void readMarshallable(BytesIn bytes) throws IORuntimeException, BufferUnderflowException, IllegalStateException {
+            read8Bit(bytes, a);
+            read8Bit(bytes, b);
+            read8Bit(bytes, c);
+            read8Bit(bytes, d);
+        }
+
+        protected void read8Bit(BytesIn bytes, Bytes a) {
+            bytes.read8bit(a);
+        }
+
+        @Override
+        public void writeMarshallable(BytesOut bytes) throws IllegalStateException, BufferOverflowException, BufferUnderflowException, ArithmeticException {
+            write8Bit(bytes, a);
+            write8Bit(bytes, b);
+            write8Bit(bytes, c);
+            write8Bit(bytes, d);
+        }
+
+        protected void write8Bit(BytesOut bytes, BytesStore a) {
+            bytes.write8bit(a);
+        }
+    }
+
+    static class ReferenceBytesFields extends DefaultBytesFields {
+        public ReferenceBytesFields() {
+        }
+
+        public ReferenceBytesFields(String a, String b, String c, String d) {
+            super(a, b, c, d);
+        }
+
+        @Override
+        protected void read8Bit(BytesIn bytes, Bytes a) {
+            int length = (int) bytes.readStopBit();
+            a.clear();
+            for (int i = 0; i < length; i++)
+                a.writeByte(bytes.readByte());
+        }
+
+        @Override
+        protected void write8Bit(BytesOut bytes, BytesStore a) {
+            final int length = a.length();
+            bytes.writeStopBit(length);
+            for (int i = 0; i < length; i++)
+                bytes.writeByte(a.readByte(i));
+        }
+    }
+
+    @Test
+    public void bytesPerformance() {
+
+    }
+}
