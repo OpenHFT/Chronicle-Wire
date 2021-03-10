b8bb3193616d26ea03d08422dcc24d693a84d67e
Peter Lawrey <peter.lawrey@chronicle.software>
2021-03-10 10:28:27 +0000

    Added performant memory access operations to use in Bytes, https://github.com/OpenHFT/Chronicle-Core/issues/209

--- src/test/java/net/openhft/chronicle/wire/bytesmarshallable/PerfRegressionTest.java
+++ src/test/java/net/openhft/chronicle/wire/bytesmarshallable/PerfRegressionTest.java
@@ -106,6 +106,7 @@ public class PerfRegressionTest {
         final Bytes bytes = Bytes.allocateElasticDirect();
         int count = 250_000;
         int repeats = 3;
+        final String cpuClass = Jvm.getCpuClass();
         for (int j = 0; j <= repeats; j++) {
             long btime = 0, dtime = 0, rtime = 0;
             if (j == 0)
@@ -147,12 +148,15 @@ public class PerfRegressionTest {
             boolean brOk = 0.65 <= b_r && b_r <= 0.87;
             if (Jvm.isJava9Plus())
                 brOk = 0.8 <= b_r && b_r <= 0.98;
+            if (cpuClass.contains("CPU E3-1"))
+                brOk = 0.9 <= b_r && b_r <= 1.1;
             if (brOk
                     && 0.39 <= d_b && d_b <= 0.6)
                 break;
             System.out.println("btime: " + btime + ", rtime: " + rtime + ", dtime: " + dtime + ", b/r: " + b_r + ", d/b: " + d_b);
-            if (j == repeats)
-                fail(Jvm.getCpuClass() + " - btime: " + btime + ", rtime: " + rtime + ", dtime: " + dtime + ", b/r: " + b_r + ", d/b: " + d_b);
+            if (j == repeats) {
+                fail(cpuClass + " - btime: " + btime + ", rtime: " + rtime + ", dtime: " + dtime + ", b/r: " + b_r + ", d/b: " + d_b);
+            }
             Jvm.pause(j * 50L);
         }
         bytes.releaseLast();
