b2736863f2eac688a6fe3c3db429f17194d942df
Peter Lawrey <peter.lawrey@chronicle.software>
2021-03-11 09:34:16 +0000

    Added performant memory access operations to use in Bytes, https://github.com/OpenHFT/Chronicle-Core/issues/209

--- src/test/java/net/openhft/chronicle/wire/bytesmarshallable/PerfRegressionTest.java
+++ src/test/java/net/openhft/chronicle/wire/bytesmarshallable/PerfRegressionTest.java
@@ -1,23 +1,24 @@
 package net.openhft.chronicle.wire.bytesmarshallable;
 
-import net.openhft.chronicle.bytes.Bytes;
-import net.openhft.chronicle.bytes.BytesIn;
-import net.openhft.chronicle.bytes.BytesOut;
-import net.openhft.chronicle.bytes.BytesStore;
+import net.openhft.chronicle.bytes.*;
 import net.openhft.chronicle.core.Jvm;
+import net.openhft.chronicle.core.OS;
 import net.openhft.chronicle.core.io.IORuntimeException;
+import net.openhft.chronicle.core.io.IOTools;
 import net.openhft.chronicle.wire.BytesInBinaryMarshallable;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.BufferOverflowException;
 import java.nio.BufferUnderflowException;
-import java.util.function.BooleanSupplier;
+import java.util.function.Predicate;
+import java.util.stream.LongStream;
 
 public class PerfRegressionTest {
 
     final String cpuClass = Jvm.getCpuClass();
-    private double b_r;
-    private double d_r;
 
     static class BytesFields extends BytesInBinaryMarshallable {
         Bytes a = Bytes.allocateElasticOnHeap();
@@ -95,113 +96,59 @@ public class PerfRegressionTest {
         }
     }
 
-    @Test
-    public void bytesPerformanceDirect() {
-        final Bytes bytes = Bytes.allocateElasticDirect();
-        doRegressionTest("direct", bytes, this::directOk);
+    static class StringFields extends BytesInBinaryMarshallable {
+        String a = "";
+        String b = "";
+        String c = "";
+        String d = "";
+
+        public StringFields() {
+        }
+
+        public StringFields(String a, String b, String c, String d) {
+            this();
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
     }
 
-    private boolean directOk() {
-        // assume it's our primary build server
-        if (cpuClass.equals("AMD Ryzen 5 3600 6-Core Processor")) {
-            boolean brOk = 0.81 <= b_r && b_r <= 0.87;
-            boolean drOk = 0.35 <= d_r && d_r <= 0.38;
-            if (Jvm.isJava9Plus()) {
-                brOk = 0.64 <= b_r && b_r <= 0.68;
-                drOk = 0.48 <= d_r && d_r <= 0.52;
-            }
-            return brOk && drOk;
-
-        } else if (cpuClass.startsWith("ARMv7")) {
-            return true;
-
-        } else if (cpuClass.contains("  E3-1270 v3 ")) {
-            boolean brOk = 1.17 <= b_r && b_r <= 1.25;
-            boolean drOk = 0.34 <= d_r && d_r <= 0.38;
-//            if (Jvm.isJava9Plus())
-//                drOk = 0.4 <= d_r && d_r <= 0.66;
-            return brOk && drOk;
-
-        } else if (cpuClass.contains("  E5-2650 v4 ")) {
-            boolean brOk = 1.08 <= b_r && b_r <= 1.11;
-            boolean drOk = 0.49 <= d_r && d_r <= 0.55;
-//            if (Jvm.isJava9Plus())
-//                drOk = 0.4 <= d_r && d_r <= 0.66;
-            return brOk && drOk;
-
-        } else if (cpuClass.contains(" i7-10710U ")) {
-            boolean brOk = 0.68 <= b_r && b_r <= 0.72;
-            if (Jvm.isJava9Plus())
-                brOk = 0.71 <= b_r && b_r <= 0.73;
-            if (brOk
-                    && 0.48 <= d_r && d_r <= 0.52)
-                return true;
-
-        } else {
-            boolean brOk = 0.65 <= b_r && b_r <= 0.87;
-            if (Jvm.isJava9Plus())
-                brOk = 0.7 <= b_r && b_r <= 0.98;
-            if (brOk
-                    && 0.39 <= d_r && d_r <= 0.61)
-                return true;
+    static class DefaultStringFields extends StringFields {
+        public DefaultStringFields() {
         }
-        return false;
+
+        public DefaultStringFields(String a, String b, String c, String d) {
+            super(a, b, c, d);
+        }
+
+        @Override
+        public void readMarshallable(BytesIn bytes) throws IORuntimeException, BufferUnderflowException, IllegalStateException {
+            a = read8Bit(bytes);
+            b = read8Bit(bytes);
+            c = read8Bit(bytes);
+            d = read8Bit(bytes);
+        }
+
+        protected @Nullable String read8Bit(BytesIn bytes) {
+            return bytes.read8bit();
+        }
+
+        @Override
+        public void writeMarshallable(BytesOut bytes) throws IllegalStateException, BufferOverflowException, BufferUnderflowException, ArithmeticException {
+            write8Bit(bytes, a);
+            write8Bit(bytes, b);
+            write8Bit(bytes, c);
+            write8Bit(bytes, d);
+        }
+
+        protected void write8Bit(BytesOut bytes, String a) {
+            bytes.write8bit(a);
+        }
     }
 
     @Test
-    public void bytesPerformanceOnHeap() {
-        final Bytes bytes = Bytes.allocateElasticOnHeap();
-        doRegressionTest("onHeap", bytes, this::onHeapOk);
-    }
-
-
-    private boolean onHeapOk() {
-        // assume it's our primary build server
-        if (cpuClass.equals("AMD Ryzen 5 3600 6-Core Processor")) {
-            boolean brOk = 0.71 <= b_r && b_r <= 0.78;
-            boolean drOk = 0.42 <= d_r && d_r <= 0.52;
-            if (Jvm.isJava9Plus()) {
-                brOk = 0.92 <= b_r && b_r <= 0.99;
-                drOk = 0.62 <= d_r && d_r <= 0.65;
-            }
-            return brOk && drOk;
-
-        } else if (cpuClass.startsWith("ARMv7")) {
-            return true;
-
-        } else if (cpuClass.contains("  E3-1270 v3 ")) {
-            boolean brOk = 0.83 <= b_r && b_r <= 0.89;
-            boolean drOk = 0.61 <= d_r && d_r <= 0.69;
-//            if (Jvm.isJava9Plus())
-//                drOk = 0.4 <= d_r && d_r <= 0.66;
-            return brOk && drOk;
-
-        } else if (cpuClass.contains("  E5-2650 v4 ")) {
-            boolean brOk = 0.80 <= b_r && b_r <= 0.86;
-            boolean drOk = 0.66 <= d_r && d_r <= 0.69;
-//            if (Jvm.isJava9Plus())
-//                drOk = 0.4 <= d_r && d_r <= 0.66;
-            return brOk && drOk;
-
-        } else if (cpuClass.contains(" i7-10710U ")) {
-            boolean brOk = 0.7 <= b_r && b_r <= 0.77;
-            boolean drOk = 0.55 <= d_r && d_r <= 0.59;
-            if (Jvm.isJava9Plus())
-                drOk = 0.62 <= d_r && d_r <= 0.66;
-            return brOk && drOk;
-
-        } else {
-            boolean brOk = 0.65 <= b_r && b_r <= 0.87;
-            if (Jvm.isJava9Plus())
-                brOk = 0.7 <= b_r && b_r <= 0.98;
-            if (brOk
-                    && 0.39 <= d_r && d_r <= 0.61)
-                return true;
-        }
-        return false;
-    }
-
-    private void doRegressionTest(String desc, Bytes bytes, BooleanSupplier test) {
+    public void regressionTests() throws FileNotFoundException {
         BytesFields bf1 = new BytesFields("12", "12345", "123456789012", "12345678901234567890123");
         BytesFields bf2 = new BytesFields();
 
@@ -211,57 +158,161 @@ public class PerfRegressionTest {
         ReferenceBytesFields rf1 = new ReferenceBytesFields("12", "12345", "123456789012", "12345678901234567890123");
         ReferenceBytesFields rf2 = new ReferenceBytesFields();
 
+        StringFields sf1 = new StringFields("12", "12345", "123456789012", "12345678901234567890123");
+        StringFields sf2 = new StringFields();
+
+        DefaultStringFields dsf1 = new DefaultStringFields("12", "12345", "123456789012", "12345678901234567890123");
+        DefaultStringFields dsf2 = new DefaultStringFields();
+
+        final Bytes direct = Bytes.allocateElasticDirect();
+        final Bytes onHeap = Bytes.allocateElasticOnHeap();
+        String file = IOTools.tempName("regressionTests");
+        try (MappedBytes mapped = MappedBytes.mappedBytes(file, OS.pageSize())) {
+            doTest("onHeap-default, direct-default, mapped-default, " +
+                            "onHeap-default-string, direct-default-string, mapped-default-string, " +
+                            "onHeap-fields, direct-fields, mapped-fields, " +
+                            "onHeap-reference, direct-reference, mapped-reference," +
+                            "onHeap-string, direct-string, mapped-string",
+                    times -> {
+                        double od = times[0];
+                        double dd = times[1];
+                        double md = times[2];
+                        double osd = times[3];
+                        double dsd = times[4];
+                        double msd = times[5];
+                        return timesOk(od, dd, md, osd, dsd, msd);
+                    },
+                    () -> {
+                        onHeap.clear();
+                        df1.writeMarshallable(onHeap);
+                        df2.readMarshallable(onHeap);
+                    },
+                    () -> {
+                        direct.clear();
+                        df1.writeMarshallable(direct);
+                        df2.readMarshallable(direct);
+                    },
+                    () -> {
+                        mapped.clear();
+                        df1.writeMarshallable(mapped);
+                        df2.readMarshallable(mapped);
+                    },
+                    () -> {
+                        onHeap.clear();
+                        dsf1.writeMarshallable(onHeap);
+                        dsf2.readMarshallable(onHeap);
+                    },
+                    () -> {
+                        direct.clear();
+                        dsf1.writeMarshallable(direct);
+                        dsf2.readMarshallable(direct);
+                    },
+                    () -> {
+                        mapped.clear();
+                        dsf1.writeMarshallable(mapped);
+                        dsf2.readMarshallable(mapped);
+                    },
+                    () -> {
+                        onHeap.clear();
+                        bf1.writeMarshallable(onHeap);
+                        bf2.readMarshallable(onHeap);
+                    },
+                    () -> {
+                        direct.clear();
+                        bf1.writeMarshallable(direct);
+                        bf2.readMarshallable(direct);
+                    },
+                    () -> {
+                        mapped.clear();
+                        bf1.writeMarshallable(mapped);
+                        bf2.readMarshallable(mapped);
+                    },
+                    () -> {
+                        onHeap.clear();
+                        rf1.writeMarshallable(onHeap);
+                        rf2.readMarshallable(onHeap);
+                    },
+                    () -> {
+                        direct.clear();
+                        rf1.writeMarshallable(direct);
+                        rf2.readMarshallable(direct);
+                    },
+                    () -> {
+                        mapped.clear();
+                        rf1.writeMarshallable(mapped);
+                        rf2.readMarshallable(mapped);
+                    },
+                    () -> {
+                        onHeap.clear();
+                        sf1.writeMarshallable(onHeap);
+                        sf2.readMarshallable(onHeap);
+                    },
+                    () -> {
+                        direct.clear();
+                        sf1.writeMarshallable(direct);
+                        sf2.readMarshallable(direct);
+                    },
+                    () -> {
+                        mapped.clear();
+                        sf1.writeMarshallable(mapped);
+                        sf2.readMarshallable(mapped);
+                    });
+        }
+        new File(file).delete();
+        onHeap.releaseLast();
+        direct.releaseLast();
+    }
+
+    private boolean timesOk(double od, double dd, double md, double osd, double dsd, double msd) {
+        // assume it's our primary build server
+        if (cpuClass.equals("AMD Ryzen 5 3600 6-Core Processor")) {
+
+        } else if (cpuClass.startsWith("ARMv7")) {
+
+        } else if (cpuClass.contains(" E3-1270 v3 ")) {
+
+        } else if (cpuClass.contains(" E5-2650 v4 ")) {
+
+        } else if (cpuClass.contains(" i7-10710U ")) {
+            return (0.47 <= od && od <= 1) // TODO FIX OnHeap so it is more reliably optimised
+                    && (0.52 <= dd && dd <= 0.64)
+                    && (0.55 <= md && md <= 0.67)
+                    && (0.46 <= osd && osd <= 1.02) // TODO FIX OnHeap so it is more reliably optimised
+                    && (0.44 <= dsd && dsd <= 0.53)
+                    && (0.41 <= msd && msd <= 0.53);
+        }
+        return false;
+    }
+
+    void doTest(String names, Predicate<double[]> check, Runnable... tests) {
+        long[] times = new long[tests.length];
         int count = 250_000;
         int repeats = 10, outlier = 10_000;
+        String[] namesArr = names.split(", ?");
         for (int j = 0; j <= repeats; j++) {
-            long btime = 0, dtime = 0, rtime = 0;
             if (j == 0)
                 count = 20_000;
             for (int i = 0; i < count; i++) {
-                long start = System.nanoTime();
-
-                bytes.clear();
-                bf1.writeMarshallable(bytes);
-                bf2.readMarshallable(bytes);
-
-                long mid1 = System.nanoTime();
-
-                bytes.clear();
-                rf1.writeMarshallable(bytes);
-                rf2.readMarshallable(bytes);
-
-                long mid2 = System.nanoTime();
-
-                bytes.clear();
-                df1.writeMarshallable(bytes);
-                df2.readMarshallable(bytes);
-
-                long end = System.nanoTime();
-
-                btime += Math.min(outlier, mid1 - start);
-                rtime += Math.min(outlier, mid2 - mid1);
-                dtime += Math.min(outlier, end - mid2);
+                for (int t = 0; t < tests.length; t++) {
+                    long start = System.nanoTime();
+                    tests[t].run();
+                    long end = System.nanoTime();
+                    times[t] += Math.min(outlier, end - start);
+                }
             }
-            btime /= count;
-            dtime /= count;
-            rtime /= count;
-            b_r = 100 * btime / rtime / 100.0;
-            d_r = 100 * dtime / rtime / 100.0;
-            if (j == 0) {
-                Thread.yield();
-                continue;
-            }
-            final String msg = cpuClass + " - " + desc + " - btime: " + btime + ", rtime: " + rtime + ", dtime: " + dtime + ", b/r: " + b_r + ", d/r: " + d_r;
-            System.out.println(msg);
-            if (test.getAsBoolean())
+            long mid = LongStream.of(times.clone()).sorted().toArray()[times.length / 2];
+            //noinspection IntegerDivisionInFloatingPointContext
+            double[] relTimes =
+                    LongStream.of(times)
+                            .mapToDouble(i -> 100L * i / mid / 100.0)
+                            .toArray();
+            System.out.print(cpuClass);
+            for (int t = 0; t < namesArr.length; t++)
+                System.out.print(", " + namesArr[t] + ": " + relTimes[t]);
+            System.out.println();
+            if (check.test(relTimes))
                 break;
-            if (j == repeats) {
-                System.out.println("FAIL");
-                // fail(msg);
-            }
             Jvm.pause(j * 50L);
         }
-        bytes.releaseLast();
     }
-
 }
