3447d6a3826fad502281b9308e838bdb91dcac88
Peter Lawrey <peter.lawrey@chronicle.software>
2021-03-17 12:12:21 +0000

    Added EmbeddedBytes for Trivially Copyable data transfer objects, https://github.com/OpenHFT/Chronicle-Bytes/issues/173

--- src/test/java/net/openhft/chronicle/wire/EmbeddedBytesMarshallableTest.java
+++ src/test/java/net/openhft/chronicle/wire/EmbeddedBytesMarshallableTest.java
@@ -1,6 +1,7 @@
 package net.openhft.chronicle.wire;
 
 import net.openhft.chronicle.bytes.*;
+import net.openhft.chronicle.bytes.internal.BytesFieldInfo;
 import net.openhft.chronicle.core.io.IORuntimeException;
 import net.openhft.chronicle.core.pool.ClassAliasPool;
 import org.junit.Test;
@@ -28,26 +29,35 @@ public class EmbeddedBytesMarshallableTest {
         assertEquals(expected, e1.toString());
         Bytes bytes = new HexDumpBytes();
         e1.writeMarshallable(bytes);
-        assertEquals("c4 5f 74 4c 1e 61 31 32 33 34 35 36 37 38 39 30\n" +
+        assertEquals("00 00 03 07 1e 61 31 32 33 34 35 36 37 38 39 30\n" +
                 "31 32 33 34 35 36 37 38 39 30 31 32 33 34 35 36\n" +
                 "37 38 39 00 17 61 31 32 33 34 35 36 37 38 39 30\n" +
-                "31 32 33 34 35 36 37 38 39 61 62 63 0b 61 31 32\n" +
-                "33 34 35 36 37 38 39 30\n", bytes.toHexString());
+                "31 32 33 34 35 36 37 38 39 61 62 63 c4 5f 74 4c\n" +
+                "0b 61 31 32 33 34 35 36 37 38 39 30\n", bytes.toHexString());
         EBM e2 = new EBM();
         e2.readMarshallable(bytes);
         assertEquals(expected, e2.toString());
         bytes.releaseLast();
     }
 
-    static class EBM extends SelfDescribingMarshallable {
+    static class SelfDescribingTriviallyCopyable extends SelfDescribingMarshallable {
+        @FieldGroup("header")
+        transient int description = BytesFieldInfo.lookup(getClass()).description();
+    }
+
+    static class EBM extends SelfDescribingTriviallyCopyable {
+        static final int DESCRIPTION = BytesFieldInfo.lookup(EBM.class).description();
         static final int[] range = BytesUtil.triviallyCopyableRange(EBM.class);
         public static final int LENGTH = range[1] - range[0];
         public static final int START = range[0];
 
         @IntConversion(Base85IntConverter.class)
         int number;
+        @FieldGroup("a")
         transient long a0, a1, a2, a3;
+        @FieldGroup("b")
         transient long b0, b1, b2;
+        @FieldGroup("c")
         transient int c0, c1, c3;
         Bytes a = Bytes.forFieldGroup(this, "a");
         Bytes b = Bytes.forFieldGroup(this, "b");
@@ -55,7 +65,10 @@ public class EmbeddedBytesMarshallableTest {
 
         @Override
         public void readMarshallable(BytesIn bytes) throws IORuntimeException, BufferUnderflowException, IllegalStateException {
-            bytes.unsafeReadObject(this, START, LENGTH);
+            int description0 = bytes.readInt();
+            if (description0 != DESCRIPTION)
+                throw new UnsupportedOperationException("TODO FIX");
+            bytes.unsafeReadObject(this, START + 4, LENGTH - 4);
         }
 
         @Override
