18f31dddfde469b53bcf56b0d7179091ead11d14
Jerry Shea <jerry.shea@chronicle.software>
2021-04-11 13:03:49 +1000

    fix BinaryWire.readingPeekYaml for padded Wire

--- src/main/java/net/openhft/chronicle/wire/BinaryWire.java
+++ src/main/java/net/openhft/chronicle/wire/BinaryWire.java
@@ -226,7 +226,7 @@ public class BinaryWire extends AbstractWire implements Wire {
         long start = readContext.start;
         if (start == -1)
             return "";
-        return Wires.fromSizePrefixedBlobs(bytes, start);
+        return Wires.fromSizePrefixedBlobs(bytes, start, usePadding());
     }
 
     @Override
