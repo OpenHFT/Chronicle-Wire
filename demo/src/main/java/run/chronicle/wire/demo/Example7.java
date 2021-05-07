96801e7e5a7b6a96fabf82a569a3e0d37853536e
foroughgoudarzi <forough.goudarzi@chronicle.software>
2021-05-07 09:28:06 +0100

    Tidy and fix typo.

--- demo/src/main/java/run/chronicle/wire/demo/Example7.java
+++ demo/src/main/java/run/chronicle/wire/demo/Example7.java
@@ -7,10 +7,10 @@ import net.openhft.chronicle.wire.WireType;
 import java.io.IOException;
 
 
-//This example shows using alias name for classes
+//This example shows how to use alias names for classes
 public class Example7 {
 
-    //Data1.class is in Alias pool but Data2.class is not.
+    //Data1.class is added to Alias pool but Data2.class is not
     static {
         ClassAliasPool.CLASS_ALIASES.addAlias(Data1.class);
     }
@@ -21,7 +21,6 @@ public class Example7 {
         data1.name = "James";
         data1.age = 20;
         data1.address = "12 Kingston, London";
-
 
         Data2 data2 = new Data2();
         data2.name = "James";
@@ -36,7 +35,6 @@ public class Example7 {
         //}
         System.out.println(data1);
 
-
         //Prints out:
         // !run.chronicle.wire.demo.Example7$Data2
         // name: james,
@@ -46,8 +44,8 @@ public class Example7 {
         System.out.println(data2);
 
 // Reading from yaml files.
-// For Data1 object alias name is used in the yaml file (cfg1.yaml). Data2 object should be loaded from a yaml file
-// with the complete name of class (including package name) otherwise you will receive Exception. See the cfg1.yaml and cfg2.yaml
+// For Data1 object, alias name is used in the yaml file (cfg1.yaml). Data2 object should be loaded from a yaml file
+// with the complete name of class (including package name) otherwise you will receive an Exception. See cfg1.yaml and cfg2.yaml
 
         Data1 o1 = WireType.TEXT.fromFile("cfg1.yaml");
 
