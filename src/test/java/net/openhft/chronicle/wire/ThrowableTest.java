8026913e1aab666eb476ea0978fa3e834f975404
Peter Lawrey <peter.lawrey@chronicle.software>
2021-07-14 10:15:29 +0100

    Reduce direct usage of Deprecated classes #204

--- src/test/java/net/openhft/chronicle/wire/ThrowableTest.java
+++ src/test/java/net/openhft/chronicle/wire/ThrowableTest.java
@@ -12,18 +12,22 @@ public class ThrowableTest extends WireTestCommon {
         for (WireType wireType : new WireType[]{WireType.TEXT, WireType.BINARY_LIGHT}) {
 
             Wire wire = wireType.apply(Bytes.allocateElasticDirect());
-            Throwable message = new Throwable("message");
-            message.initCause(new Throwable("cause"));
-            wire.getValueOut()
-                    .object(message);
-            if (wireType == WireType.TEXT)
+            try (DocumentContext dc = wire.writingDocument()) {
+                Throwable message = new Throwable("message");
+                message.initCause(new Throwable("cause"));
+                wire.getValueOut()
+                        .object(message);
+            }
+/*            if (wireType == WireType.TEXT)
                 System.out.println(wire);
             else
-                System.out.println(wire.bytes().toHexString());
+                System.out.println(wire.bytes().toHexString()+"\n"+Wires.fromSizePrefixedBlobs(wire.bytes()));*/
 
-            Throwable t = (Throwable) wire.getValueIn().object();
-            assertEquals("message", t.getMessage());
-            assertTrue(t.getStackTrace()[0].toString().startsWith("net.openhft.chronicle.wire.ThrowableTest.writeReadThrowable(ThrowableTest.java"));
+            try (DocumentContext dc = wire.readingDocument()) {
+                Throwable t = (Throwable) wire.getValueIn().object();
+                assertEquals("message", t.getMessage());
+                assertTrue(t.getStackTrace()[0].toString().startsWith("net.openhft.chronicle.wire.ThrowableTest.writeReadThrowable(ThrowableTest.java"));
+            }
             wire.bytes().releaseLast();
         }
     }
