c3627105587920fde7c7c5eb0450f5a249c16ff9
Jerry Shea <jerry.shea@chronicle.software>
2021-08-10 08:08:46 +1000

    additional test for #299. Belt and braces

--- src/test/java/net/openhft/chronicle/wire/DoubleTest.java
+++ src/test/java/net/openhft/chronicle/wire/DoubleTest.java
@@ -1,13 +1,18 @@
 package net.openhft.chronicle.wire;
 
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.core.Maths;
+import net.openhft.chronicle.core.io.UnsafeText;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static net.openhft.chronicle.core.pool.ClassAliasPool.CLASS_ALIASES;
 import static net.openhft.chronicle.wire.Marshallable.fromString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
-public class DoubleTest {
+// see also UnsafeTextBytesTest
+public class DoubleTest extends WireTestCommon {
 
     static class TwoDoubleDto extends SelfDescribingMarshallable {
         double price;
@@ -29,4 +34,20 @@ public class DoubleTest {
         Assert.assertEquals(EXPECTED, twoDoubleDto.toString());
     }
 
+    @Test
+    public void testManyDoubles() {
+        final Bytes<?> bytes = Bytes.elasticByteBuffer();
+        final long address = bytes.clear().addressForRead(0);
+
+        for (double aDouble = -1; aDouble < 1; aDouble += 0.00001) {
+            bytes.clear();
+            aDouble = Maths.round6(aDouble);
+            final long end = UnsafeText.appendDouble(address, aDouble);
+            bytes.readLimit(end - address);
+            double d2 = bytes.parseDouble();
+            assertEquals(aDouble, d2, Math.ulp(aDouble));
+            final String message = bytes.toString();
+            assertFalse(message + " has trailing 0", message.endsWith("0"));
+        }
+    }
 }
