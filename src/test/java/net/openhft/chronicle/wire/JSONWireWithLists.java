7626c539043e6533b22a2dc5a84c63438515d96a
Rob Austin <rob.austin@boundedbuffer.co.uk>
2021-10-13 16:56:00 +0100

    added failing test -> net.openhft.chronicle.wire.JSONWireWithLists

---
+++ src/test/java/net/openhft/chronicle/wire/JSONWireWithLists.java
@@ -0,0 +1,55 @@
+package net.openhft.chronicle.wire;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * relates to https://github.com/OpenHFT/Chronicle-Wire/issues/324
+ */
+@RunWith(value = Parameterized.class)
+public class JSONWireWithLists {
+
+    private final boolean useTypes;
+
+    @Parameterized.Parameters(name = "useTypes={0}")
+    public static Collection<Object[]> wireTypes() {
+        return Arrays.asList(
+                new Object[]{true},
+                new Object[]{false}
+        );
+    }
+
+    public JSONWireWithLists(boolean useTypes) {
+        this.useTypes = useTypes;
+    }
+
+
+    static class F1 {
+        private String surname;
+        private int car;
+
+        public F1(String surname, int car) {
+            this.surname = surname;
+            this.car = car;
+        }
+    }
+
+    @Test
+    public void test() {
+
+        final JSONWire jsonWire = new JSONWire()
+                .useTypes(useTypes);
+
+        final List<F1> drivers = Arrays.asList(new F1("Hamilton", 44), new F1("Verstappen", 33));
+        jsonWire.getValueOut().object(drivers);
+
+        final String actual = jsonWire.getValueIn().object().toString();
+        Assert.assertEquals("[{surname=Hamilton, car=44}, {surname=Verstappen, car=33}]", actual);
+    }
+}
