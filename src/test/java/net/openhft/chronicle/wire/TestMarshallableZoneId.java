fefff4e5dce8a7c964520f4d5641d970164873de
Rob Austin <rob.austin@boundedbuffer.co.uk>
2021-11-18 20:09:31 +0000

    added failing test - TestMarshallableZoneId

---
+++ src/test/java/net/openhft/chronicle/wire/TestMarshallableZoneId.java
@@ -0,0 +1,44 @@
+package net.openhft.chronicle.wire;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.ZoneId;
+
+public class TestMarshallableZoneId {
+
+    public static class MySelfDescribingMarshallable extends SelfDescribingMarshallable {
+        ZoneId zoneId;
+    }
+
+    @Test
+    public void testMySelfDescribingMarshallable() {
+
+        final MySelfDescribingMarshallable expected = new MySelfDescribingMarshallable();
+        expected.zoneId = ZoneId.of("UTC");
+
+        JSONWire jsonWire = new JSONWire();
+        jsonWire.getValueOut().object(expected);
+
+        final MySelfDescribingMarshallable actual = jsonWire.getValueIn().object(MySelfDescribingMarshallable.class);
+        Assert.assertEquals(expected, actual);
+    }
+
+    public static class MyAbstractMarshallableCfg extends AbstractMarshallableCfg {
+        ZoneId zoneId;
+    }
+
+    @Test
+    public void testMyAbstractMarshallableCfg() {
+
+        final MyAbstractMarshallableCfg expected = new MyAbstractMarshallableCfg();
+        expected.zoneId = ZoneId.of("UTC");
+
+        JSONWire jsonWire = new JSONWire();
+        jsonWire.getValueOut().object(expected);
+
+        final MyAbstractMarshallableCfg actual = jsonWire.getValueIn().object(MyAbstractMarshallableCfg.class);
+        Assert.assertEquals(expected, actual);
+    }
+
+}
