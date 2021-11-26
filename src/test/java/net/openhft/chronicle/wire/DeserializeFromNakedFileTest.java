6f6e6023c67aa443fed74bb6453ae2accb572509
Ilya Kaznacheev <ilya.kaznacheev@chronicle.software>
2021-11-26 11:07:04 +0300

    Test for deserializing POJO from yaml file

---
+++ src/test/java/net/openhft/chronicle/wire/DeserializeFromNakedFileTest.java
@@ -0,0 +1,44 @@
+package net.openhft.chronicle.wire;
+
+import net.openhft.chronicle.bytes.BytesMarshallable;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeserializeFromNakedFileTest {
+    @Test
+    public void testPOJO() throws IOException {
+        PlainOldJavaClass res = Marshallable.fromFile(PlainOldJavaClass.class, "naked.yaml");
+
+        assertEquals(20, res.heartBtInt);
+    }
+
+    @Test
+    public void testSelfDescribing() throws IOException {
+        SelfDescribingClass res = Marshallable.fromFile(SelfDescribingClass.class, "naked.yaml");
+
+        assertEquals(20, res.heartBtInt);
+    }
+
+//    @Test
+//    public void testBytes() throws IOException {
+//        BytesClass res = Marshallable.fromFile(BytesClass.class, "naked.yaml");
+//
+//        assertEquals(20, res.heartBtInt);
+//    }
+
+    private static class PlainOldJavaClass {
+        public int heartBtInt;
+    }
+
+
+    private static class SelfDescribingClass extends SelfDescribingMarshallable {
+        public int heartBtInt;
+    }
+
+//    private static class BytesClass implements BytesMarshallable {
+//        public int heartBtInt;
+//    }
+}
