ad39990ecb205098a4b83a31745693db5528f5e6
Jerry Shea <jerry.shea@chronicle.software>
2021-12-29 11:56:53 +1100

    test for #370

--- src/test/java/net/openhft/chronicle/wire/Base40LongConverterTest.java
+++ src/test/java/net/openhft/chronicle/wire/Base40LongConverterTest.java
@@ -1,5 +1,6 @@
 package net.openhft.chronicle.wire;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -19,6 +20,18 @@ public class Base40LongConverterTest extends WireTestCommon {
         for (String s : ",a,0,zz,99,abcdef,012345,zzzzzzzzzzzz,999999999999".split(",")) {
             assertEquals(s, bic.asString(bic.parse(s)));
         }
+    }
 
+    @Ignore("https://github.com/OpenHFT/Chronicle-Wire/issues/370")
+    @Test
+    public void longConversion() {
+        Sample sample = new Sample();
+        sample.strategyId = Base40LongConverter.INSTANCE.parse("TEST");
+        assertEquals("TODO", sample.toString());
+    }
+
+    private static class Sample extends SelfDescribingMarshallable {
+        @LongConversion(Base40LongConverter.class)
+        public long strategyId;
     }
 }
