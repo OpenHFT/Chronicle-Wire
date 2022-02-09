ac3fa6274a113352412345ff1f4fc9446b9aca17
Peter Lawrwy <peter.lawrey@chronicle.software>
2022-02-09 11:28:11 +0000

    Added examples with queue

---
+++ src/test/java/net/openhft/chronicle/wire/marshallable/TriviallyCopyableMarketData.java
@@ -0,0 +1,34 @@
+package net.openhft.chronicle.wire.marshallable;
+
+import net.openhft.chronicle.bytes.BytesIn;
+import net.openhft.chronicle.bytes.BytesOut;
+import net.openhft.chronicle.bytes.BytesUtil;
+import net.openhft.chronicle.wire.*;
+
+public class TriviallyCopyableMarketData extends BytesInBinaryMarshallable {
+    private static final int[] START_END = BytesUtil.triviallyCopyableRange(TriviallyCopyableMarketData.class);
+    private static final int START = START_END[0];
+    private static final int LENGTH = START_END[1] - START_END[0];
+
+    @LongConversion(Base85LongConverter.class)
+    long securityId;
+
+    @LongConversion(MicroTimestampLongConverter.class)
+    long time;
+
+    double bid0, bid1, bid2, bid3;
+    double bidQty0, bidQty1, bidQty2, bidQty3;
+
+    double ask0, ask1, ask2, ask3;
+    double askQty0, askQty1, askQty2, askQty3;
+
+    @Override
+    public void readMarshallable(BytesIn bytes) {
+        bytes.unsafeReadObject(this, START, LENGTH);
+    }
+
+    @Override
+    public void writeMarshallable(BytesOut bytes) {
+        bytes.unsafeWriteObject(this, START, LENGTH);
+    }
+}
