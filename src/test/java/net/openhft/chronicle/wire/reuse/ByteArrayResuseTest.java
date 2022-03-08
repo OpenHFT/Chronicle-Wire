499ed2c45a2f2df1bd671a243a151eecf9f59a82
Peter Lawrey <peter.lawrey@chronicle.software>
2022-03-08 13:50:14 +0000

    Improve efficiency of (de)marshalling byte[] #405

--- src/test/java/net/openhft/chronicle/wire/reuse/ByteArrayResuseTest.java
+++ src/test/java/net/openhft/chronicle/wire/reuse/ByteArrayResuseTest.java
@@ -13,6 +13,37 @@ import static org.junit.Assert.assertSame;
 public class ByteArrayResuseTest {
     @Test
     public void writeReadBytesArray() {
+        SELF_DESCRIBING = true;
+        doWriteReadBytesArray("" +
+                "c4 64 61 74 61                                  # data:\n" +
+                "80 22                                           # Data\n" +
+                "   c9 74 69 6d 65 73 74 61 6d 70                   # timestamp:\n" +
+                "   a6 d2 02 96 49                                  # 1234567890\n" +
+                "   c5 62 79 74 65 73 80 0b 8a 01 02 03 04 05 06 07 # bytes:\n" +
+                "   08 09 00\n" +
+                "c4 64 61 74 61                                  # data:\n" +
+                "80 22                                           # Data\n" +
+                "   c9 74 69 6d 65 73 74 61 6d 70                   # timestamp:\n" +
+                "   a6 d2 02 96 49                                  # 1234567890\n" +
+                "   c5 62 79 74 65 73 80 0b 8a 01 02 03 04 05 06 07 # bytes:\n" +
+                "   08 09 00\n");
+    }
+
+    @Test
+    public void writeReadBytesArrayBinary() {
+        SELF_DESCRIBING = false;
+        doWriteReadBytesArray("" +
+                "c4 64 61 74 61                                  # data:\n" +
+                "80 16                                           # Data\n" +
+                "   d2 02 96 49 00 00 00 00                         # timestamp\n" +
+                "   0a 00 00 00 01 02 03 04 05 06 07 08 09 00       # bytes\n" +
+                "c4 64 61 74 61                                  # data:\n" +
+                "80 16                                           # Data\n" +
+                "   d2 02 96 49 00 00 00 00                         # timestamp\n" +
+                "   0a 00 00 00 01 02 03 04 05 06 07 08 09 00       # bytes\n");
+    }
+
+    public void doWriteReadBytesArray(String expected) {
         Data data = new Data();
         data.timestamp = 1234567890L;
         byte[] bytes = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
@@ -22,19 +53,7 @@ public class ByteArrayResuseTest {
         wire.write("data").object(Data.class, data);
         wire.write("data").object(Data.class, data);
 
-        assertEquals("" +
-                        "c4 64 61 74 61                                  # data:\n" +
-                        "80 22                                           # Data\n" +
-                        "   c9 74 69 6d 65 73 74 61 6d 70                   # timestamp:\n" +
-                        "   a6 d2 02 96 49                                  # 1234567890\n" +
-                        "   c5 62 79 74 65 73 80 0b 8a 01 02 03 04 05 06 07 # bytes:\n" +
-                        "   08 09 00\n" +
-                        "c4 64 61 74 61                                  # data:\n" +
-                        "80 22                                           # Data\n" +
-                        "   c9 74 69 6d 65 73 74 61 6d 70                   # timestamp:\n" +
-                        "   a6 d2 02 96 49                                  # 1234567890\n" +
-                        "   c5 62 79 74 65 73 80 0b 8a 01 02 03 04 05 06 07 # bytes:\n" +
-                        "   08 09 00\n",
+        assertEquals(expected,
                 wire.bytes().toHexString());
 
         Data data2 = new Data();
@@ -46,9 +65,16 @@ public class ByteArrayResuseTest {
         assertSame(bytes2, data2.bytes);
     }
 
+    static boolean SELF_DESCRIBING;
+
     static class Data extends SelfDescribingMarshallable {
         long timestamp;
         byte[] bytes;
+
+        @Override
+        public boolean usesSelfDescribingMessage() {
+            return SELF_DESCRIBING;
+        }
 
         @Override
         public BinaryLengthLength binaryLengthLength() {
