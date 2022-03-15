439933f9257779161c4b7b4164747e60fbfe5e8b
Rob Austin <rob.austin@boundedbuffer.co.uk>
2022-03-15 16:45:14 +0000

    changed to ->  private final Wire wire = new BinaryWire(new HexDumpBytes());

--- src/test/java/net/openhft/chronicle/wire/examples/MessageRoutingExample.java
+++ src/test/java/net/openhft/chronicle/wire/examples/MessageRoutingExample.java
@@ -1,12 +1,15 @@
 package net.openhft.chronicle.wire.examples;
 
 import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.bytes.HexDumpBytes;
 import net.openhft.chronicle.bytes.MethodReader;
+import net.openhft.chronicle.bytes.util.BinaryLengthLength;
 import net.openhft.chronicle.wire.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static net.openhft.chronicle.bytes.util.BinaryLengthLength.LENGTH_8BIT;
 import static net.openhft.chronicle.core.pool.ClassAliasPool.CLASS_ALIASES;
 
 public class MessageRoutingExample {
@@ -16,9 +19,14 @@ public class MessageRoutingExample {
         CLASS_ALIASES.addAlias(Product.class, ProductHandler.class);
     }
 
+    public static void main(String[] args) {
+        new MessageRoutingExample().demo();
+    }
+
     /**
      * used to route products to the destination by providing the appropriate ProductHandler
      */
+    @FunctionalInterface
     interface Routing {
         ProductHandler to(String destination);
     }
@@ -27,6 +35,7 @@ public class MessageRoutingExample {
     /**
      * Implement this to read the process of the product events.
      */
+    @FunctionalInterface
     interface ProductHandler {
         void product(Product product);
     }
@@ -40,15 +49,16 @@ public class MessageRoutingExample {
         public Product(String name) {
             this.name = name;
         }
+
+        @Override
+        public BinaryLengthLength binaryLengthLength() {
+            return LENGTH_8BIT;
+        }
     }
 
-
-    public static void main(String[] args) {
-        new MessageRoutingExample().demo();
-    }
 
     // the serialized data gets written to 'wire'
-    private final Wire wire = new JSONWire(Bytes.allocateElasticOnHeap()).useTypes(true);
+    private final Wire wire = new BinaryWire(new HexDumpBytes());
 
     private void demo() {
 
@@ -64,8 +74,8 @@ public class MessageRoutingExample {
         routing.to("France").product(new Product("Cheese"));
         routing.to("Russia").product(new Product("Vodka"));
 
-        System.out.println(wire);
-        //  System.out.println(wire.bytes().toHexString());
+        // System.out.println(wire);
+        System.out.println(wire.bytes().toHexString());
 
         MethodReader methodReader = wire.methodReader((Routing) destinationMap::get);
 
@@ -74,7 +84,6 @@ public class MessageRoutingExample {
             // true if a message was read
             success = methodReader.readOne();
         } while (success);
-
 
     }
 
