3454b40fffbd8c122a9d1b1010a903dd6a805240
Peter Lawrwy <peter.lawrey@chronicle.software>
2022-04-20 12:32:26 +0100

    Check a Comparable type can be deserialized as a Scalar before writing it as a Scalar type, #438

--- src/test/java/net/openhft/chronicle/wire/serializable/MonetaTest.java
+++ src/test/java/net/openhft/chronicle/wire/serializable/MonetaTest.java
@@ -3,24 +3,43 @@ package net.openhft.chronicle.wire.serializable;
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.wire.TextWire;
 import net.openhft.chronicle.wire.Wire;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
-import javax.money.CurrencyQueryBuilder;
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-import java.util.Collection;
+import java.io.Serializable;
+import java.util.Currency;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import static org.junit.Assert.assertEquals;
 
 public class MonetaTest {
     @Test
     public void monetary() {
-        Collection<CurrencyUnit> allCurrencies = Monetary.getCurrencies(CurrencyQueryBuilder.of().build());
+        SortedSet<NonScalarComparable> set = new TreeSet<>();
+        for (String curr : "GBP,USD,EUR,AUD".split(","))
+            set.add(new NonScalarComparable(Currency.getInstance(curr)));
+
         Wire wire = new TextWire(Bytes.allocateElasticOnHeap());
         wire.write("currencies")
-                .object(allCurrencies);
+                .object(set);
 //        System.out.println(wire);
-        Collection<CurrencyUnit> allCurrencies2 = wire.read("currencies").object(Collection.class);
-        assertEquals(allCurrencies, allCurrencies2);
+        SortedSet<NonScalarComparable> set2 = wire
+                .read("currencies")
+                .object(SortedSet.class);
+        assertEquals(set, set2);
+    }
+
+    public static class NonScalarComparable implements Serializable, Comparable<NonScalarComparable> {
+        final Currency currency;
+
+        public NonScalarComparable(Currency currency) {
+            this.currency = currency;
+        }
+
+        @Override
+        public int compareTo(@NotNull MonetaTest.NonScalarComparable o) {
+            return currency.getDisplayName().compareTo(o.currency.getDisplayName());
+        }
     }
 }
