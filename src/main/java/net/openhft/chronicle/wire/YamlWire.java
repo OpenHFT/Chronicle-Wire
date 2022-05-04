edf88344abcd3df3a0de9fe869700ed8d020c572
Peter K Lawrey <peter.lawrey@higherfrequencytrading.com>
2022-05-04 10:11:14 +0100

    Check a Comparable type can be deserialized as a Scalar before writing it as a Scalar type, #438 (#439)
    * Check a Comparable type can be deserialized as a Scalar before writing it as a Scalar type, #438
    
    * Check a Comparable type can be deserialized as a Scalar before writing it as a Scalar type, #438
    
    * Check a Comparable type can be deserialized as a Scalar before writing it as a Scalar type, #438
    
    * Add @Nullable to ValueIn.object arg (#436)
    
    * Added an example for documentation
    
    * Added an example for documentation
    
    * Make copyTo work from JSONWire without types , #446
    
    Co-authored-by: Peter Lawrwy <peter.lawrey@chronicle.software>
    Co-authored-by: Ivan Rakov <ivan.glukos@gmail.com>

--- src/main/java/net/openhft/chronicle/wire/YamlWire.java
+++ src/main/java/net/openhft/chronicle/wire/YamlWire.java
@@ -2000,6 +2000,9 @@ public class YamlWire extends AbstractWire implements Wire {
             switch (yt.current()) {
                 default:
                     throw new UnsupportedOperationException(yt.toString());
+                case DIRECTIVES_END:
+                    yt.next();
+                    return getBracketType();
                 case MAPPING_START:
                     return BracketType.MAP;
                 case SEQUENCE_START:
