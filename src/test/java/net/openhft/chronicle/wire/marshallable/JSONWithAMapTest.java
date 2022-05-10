da7db92f2f5130a9c4dff835f18abeac66686405
Peter Lawrey <peter.lawrey@chronicle.software>
2022-05-10 15:59:16 +0100

    JsonWire : serialising a DTO the number of '{' and '}' should be equal https://github.com/OpenHFT/Chronicle-Wire/issues/467

---
+++ src/test/java/net/openhft/chronicle/wire/marshallable/JSONWithAMapTest.java
@@ -0,0 +1,105 @@
+package net.openhft.chronicle.wire.marshallable;
+
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.bytes.OnHeapBytes;
+import net.openhft.chronicle.wire.Marshallable;
+import net.openhft.chronicle.wire.SelfDescribingMarshallable;
+import net.openhft.chronicle.wire.Wire;
+import net.openhft.chronicle.wire.WireType;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static net.openhft.chronicle.core.pool.ClassAliasPool.CLASS_ALIASES;
+
+public class JSONWithAMapTest {
+    @Test
+    public void test1() {
+        final String expected = "{\"@ResponseItem\":{\"index\":\"4ab100000005\",\"key\":\"seqNumber\",\"payload\":null}}";
+
+        final String input = "!ResponseItem {\n" +
+                "  index: \"4ab100000005\",\n" +
+                "  key: seqNumber,\n" +
+                "}";
+
+        doTest(expected, input);
+    }
+
+    @Test
+    public void test2() {
+        final String input = "!ResponseItem {\n" +
+                "  index: \"4ab100000005\",\n" +
+                "  key: seqNumber,\n" +
+                "  payload: {\n" +
+                "  }\n" +
+                "}";
+        final String expected = "{\"@ResponseItem\":{\"index\":\"4ab100000005\",\"key\":\"seqNumber\",\"payload\":{}}}";
+
+        doTest(expected, input);
+    }
+
+    @Test
+    public void test5() {
+
+        final String input = "!ResponseItem {\n" +
+                "  index: \"4ab100000005\",\n" +
+                "  key: seqNumber,\n" +
+                "  payload: {\n" +
+                "    eventId: periodicUpdate,\n" +
+                "    eventTime: 1652109920838805734,\n" +
+                "    seqNumbers: [\n" +
+                "      {\n" +
+                "        sessionID: {\n" +
+                "          localCompID: SERVER,\n" +
+                "          remoteCompID: CLIENT,\n" +
+                "          localSubID: !!null \"\",\n" +
+                "          remoteSubID: !!null \"\"\n" +
+                "        },\n" +
+                "        rSeq: !short 1517,\n" +
+                "        wSeq: !short 1519,\n" +
+                "        isActive: true,\n" +
+                "        isConnected: false\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  }\n" +
+                "}";
+        final String expected = "{\"@ResponseItem\":{\"index\":\"4ab100000005\",\"key\":\"seqNumber\",\"payload\":{\"eventId\":\"periodicUpdate\",\"eventTime\":1652109920838805734,\"seqNumbers\":[ {\"sessionID\":{\"localCompID\":\"SERVER\",\"remoteCompID\":\"CLIENT\",\"localSubID\":null,\"remoteSubID\":null},\"rSeq\":1517,\"wSeq\":1519,\"isActive\":true,\"isConnected\":false} ]}}}";
+
+        doTest(expected, input);
+    }
+
+    private void doTest(String expected, String input) {
+        CLASS_ALIASES.addAlias(ResponseItem.class);
+        ResponseItem responseItem = Marshallable.fromString(ResponseItem.class, input);
+
+        OnHeapBytes buffer = Bytes.allocateElasticOnHeap();
+        final Wire jsonWire = WireType.JSON_ONLY.apply(buffer);
+        jsonWire.getValueOut().object(responseItem);
+
+        String actual = buffer.toString();
+
+        int openBracket = 0;
+        int closeBracket = 0;
+        for (int i = 0; i < actual.length(); i++) {
+            if (actual.charAt(i) == '{')
+                openBracket++;
+            if (actual.charAt(i) == '}')
+                closeBracket++;
+        }
+
+        // check the number of '{' match the number of '}'
+        Assert.assertTrue("openBracket=" + openBracket + ",closeBracket=" + closeBracket, openBracket == closeBracket);
+
+        // DON'T CHANGE THE EXPECTED JSON IT IS CORRECT ! - please use this website to validate the json - https://jsonformatter.org
+        Assert.assertEquals(expected, actual);
+    }
+
+    static class ResponseItem extends SelfDescribingMarshallable {
+        private final Bytes<?> key = Bytes.elasticByteBuffer();
+        @NotNull String index;
+        private Object payload;
+    }
+}
+
+
+
