3e9faab94b1bc069e6a10200a5e85131b2234453
Peter Lawrey <peter.lawrey@chronicle.software>
2022-08-02 07:52:23 +0100

    Ignore net.openhft.chronicle.core.io.ClosedIORuntimeException: Closed

--- src/test/java/net/openhft/chronicle/wire/channel/echo/EchoHandlerTest.java
+++ src/test/java/net/openhft/chronicle/wire/channel/echo/EchoHandlerTest.java
@@ -37,7 +37,7 @@ public class EchoHandlerTest extends WireTestCommon {
 
     @Test
     public void serverBuffered() {
-        ignoreException("net.openhft.chronicle.core.io.ClosedIORuntimeException: Closed");
+        ignoreException("Closed");
         if (Jvm.isArm()) {
             ignoreException("Using Pauser.balanced() as not enough processors");
             ignoreException("bgWriter died");
