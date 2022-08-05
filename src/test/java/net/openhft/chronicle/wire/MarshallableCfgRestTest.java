946c7182b20ad987030cb42a7b647ff441ec4bae
Rob Austin <rob.austin@boundedbuffer.co.uk>
2022-08-05 17:00:02 +0100

    #547 renamed field

--- src/test/java/net/openhft/chronicle/wire/MarshallableCfgRestTest.java
+++ src/test/java/net/openhft/chronicle/wire/MarshallableCfgRestTest.java
@@ -17,8 +17,8 @@ public class MarshallableCfgRestTest {
 
         Engine engine;
 
-        public Boat(Engine dog) {
-            this.engine = dog;
+        public Boat(Engine engine) {
+            this.engine = engine;
         }
     }
 
