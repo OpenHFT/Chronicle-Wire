aed6e98cb5bdb5caaa8d75b00fd2e55832effb86
Rob Austin <rob.austin@boundedbuffer.co.uk>
2022-08-05 16:58:16 +0100

    #547 Ensure objects that extends AbstractMarshallableCfg are reset when calling valueIn.object(Object, Class)} #547

---
+++ src/test/java/net/openhft/chronicle/wire/MarshallableCfgRestTest.java
@@ -0,0 +1,42 @@
+package net.openhft.chronicle.wire;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MarshallableCfgRestTest {
+
+    public static class Engine extends AbstractMarshallableCfg {
+        public boolean isItElectric;
+
+        public Engine(boolean isItElectric) {
+            this.isItElectric = isItElectric;
+        }
+    }
+
+    public static class Boat extends SelfDescribingMarshallable {
+
+        Engine engine;
+
+        public Boat(Engine dog) {
+            this.engine = dog;
+        }
+    }
+
+
+    /**
+     * tests that objects that extends AbstractMarshallableCfg are reset when calling {@link ValueIn#object(Object, Class)}
+     */
+    @Test
+    public void test() {
+        Boat k9f = new Boat(new Engine(false));
+
+        Wire w = new JSONWire();
+        w.getValueOut().object(k9f);
+
+        Boat using = new Boat(new Engine(true));
+
+        Boat boat = w.getValueIn().object(using, Boat.class);
+        Assert.assertFalse(boat.engine.isItElectric);
+    }
+
+}
