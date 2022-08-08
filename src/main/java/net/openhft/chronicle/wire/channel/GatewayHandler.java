2cf0c88da0566eb9f1092a19cfc5882d2cb45544
Peter Lawrey <peter.lawrey@chronicle.software>
2022-08-08 16:55:07 +0100

    Add sessionName #550

--- src/main/java/net/openhft/chronicle/wire/channel/GatewayHandler.java
+++ src/main/java/net/openhft/chronicle/wire/channel/GatewayHandler.java
@@ -8,22 +8,13 @@
 
 package net.openhft.chronicle.wire.channel;
 
-import net.openhft.chronicle.core.io.ClosedIORuntimeException;
-import net.openhft.chronicle.wire.DocumentContext;
-
 /**
- * This handler leaves the default behaviour to the Gateway
+ * This handler leaves the default behaviour to the Gateway.
+ * <p>
+ * If the gateway doesn't replace it with the Handler the gateway supports, an error is returned
  */
-public class GatewayHandler extends AbstractHandler<GatewayHandler> {
-    @Override
-    public void run(ChronicleContext context, ChronicleChannel channel) throws ClosedIORuntimeException {
-        try (DocumentContext dc = channel.writingDocument()) {
-            dc.wire().write("error").text("No default handler for the gateway");
-        }
-    }
-
-    @Override
-    public ChronicleChannel asInternalChannel(ChronicleContext context, ChronicleChannelCfg channelCfg) {
-        throw new UnsupportedOperationException();
+public class GatewayHandler extends ErrorReplyHandler {
+    public GatewayHandler() {
+        errorMsg("No default handler for the gateway");
     }
 }
