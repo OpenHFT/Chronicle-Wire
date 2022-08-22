280c362634a9b756a43eccea1fd20579cf45467a
Peter Lawrey <peter.lawrey@chronicle.software>
2022-08-22 12:14:06 +0100

    Add multiple clients to PerfThroughputMain

--- src/test/java/net/openhft/chronicle/wire/channel/PerfThroughputMain.java
+++ src/test/java/net/openhft/chronicle/wire/channel/PerfThroughputMain.java
@@ -9,96 +9,85 @@
 package net.openhft.chronicle.wire.channel;
 
 import net.openhft.chronicle.bytes.Bytes;
-import net.openhft.chronicle.bytes.MethodReader;
 import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.wire.DocumentContext;
-import net.openhft.chronicle.wire.channel.echo.DummyData;
-import net.openhft.chronicle.wire.channel.echo.EchoHandler;
+import net.openhft.chronicle.wire.channel.echo.EchoNHandler;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
 
 public class PerfThroughputMain {
     static final String URL = System.getProperty("url", "tcp://:1248");
     static final int RUN_TIME = Integer.getInteger("runTime", 5);
-    static final boolean METHOD_RW = Jvm.getBoolean("method.rw");
+    static final int CLIENTS = Integer.getInteger("clients", 1);
 
     public static void main(String[] args) {
         System.out.println("-Durl=" + URL + " " +
                 "-DrunTime=" + RUN_TIME + " " +
-                "-Dmethod.rw=" + METHOD_RW);
+                "-Dclients=" + CLIENTS);
         try (ChronicleContext context = ChronicleContext.newContext(URL)) {
-            EchoHandler echoHandler = new EchoHandler();
+            EchoNHandler echoHandler = new EchoNHandler();
+            echoHandler.times(CLIENTS);
             final ChronicleChannelSupplier supplier = context.newChannelSupplier(echoHandler);
             echoHandler.buffered(true);
-            try (ChronicleChannel channel2 = supplier.buffered(true).get()) {
-                doTest("buffered", channel2);
-            }
+            supplier.buffered(true);
+            doTest("buffered", supplier.buffered(true));
 
             echoHandler.buffered(false);
-            try (ChronicleChannel channel = supplier.get()) {
-                doTest("unbuffered", channel);
-            }
+            doTest("unbuffered", supplier.buffered(false));
         }
     }
 
-    private static void doTest(String desc, ChronicleChannel channel) {
-        DummyData data = new DummyData();
+    private static void doTest(String desc, ChronicleChannelSupplier channelSupplier) {
+        InternalChronicleChannel[] clients = new InternalChronicleChannel[CLIENTS];
+        for (int i = 0; i < CLIENTS; i++)
+            clients[i] = (InternalChronicleChannel) channelSupplier.get();
+
         for (int size = 32 << 10; size >= 8; size /= 2) {
-            data.data(new byte[size - Long.BYTES]);
             long start = System.currentTimeMillis();
             long end = start + RUN_TIME * 1000L;
-            final Echoing echoing = channel.methodWriter(Echoing.class);
-            long count = 0;
-            AtomicLong unread = new AtomicLong(0);
-            int window = (128 << 20) / size;
-            if (METHOD_RW) {
-                MethodReader reader = channel.methodReader((Echoing) d -> {
-                    unread.getAndDecrement();
-                });
-                do {
-                    echoing.echo(data);
-                    unread.getAndIncrement();
-                    count++;
-                    do {
-                        reader.readOne();
-                    } while (unread.get() > window);
-                } while (System.currentTimeMillis() < end);
+            int window = (16 << 20) / size / CLIENTS;
+            AtomicLong totalRead = new AtomicLong(0);
+            int finalSize = size;
+            Stream.of(clients)
+                    .parallel()
+                    .forEach(icc -> {
+                        int written = 0, read = 0;
 
-                do {
-                    reader.readOne();
-                } while (unread.get() > 0);
+                        do {
+                            final Bytes<?> bytes = icc.acquireProducer().bytes();
+                            bytes.writeInt(finalSize);
+                            for (int i = 0; i < finalSize; i += 8)
+                                bytes.writeLong(0L);
+                            icc.releaseProducer();
 
-            } else {
-                InternalChronicleChannel icc = (InternalChronicleChannel) channel;
-                do {
-                    final Bytes<?> bytes = icc.acquireProducer().bytes();
-                    bytes.writeInt(size);
-                    for (int i = 0; i < size; i += 8)
-                        bytes.writeLong(0L);
-                    icc.releaseProducer();
+                            // due to the multiplier in the EchoNHandler
+                            written += CLIENTS;
 
-                    unread.getAndIncrement();
-                    count++;
-                    do {
-                        try (DocumentContext dc = channel.readingDocument()) {
-                            if (dc.isPresent())
-                                unread.getAndDecrement();
-                        }
-                    } while (unread.get() > window);
-                } while (System.currentTimeMillis() < end);
+                            read = readUpto(window, icc, written, read);
+                        } while (System.currentTimeMillis() < end);
 
-                do {
-                    try (DocumentContext dc = channel.readingDocument()) {
-                        if (dc.isPresent())
-                            unread.getAndDecrement();
-                    }
-                } while (unread.get() > 0);
-            }
+                        readUpto(0, icc, written, read);
+                        totalRead.addAndGet(read);
+                    });
+
+            long count = totalRead.get();
             long time = System.currentTimeMillis() - start;
             long totalBytes = size * count;
             long MBps = totalBytes / time / (1_000_000 / 1_000);
             long rate = count * 1000 / time;
             System.out.println("desc: " + desc + ", size: " + size + ", MBps: " + MBps + ", mps: " + rate);
         }
+    }
+
+    private static int readUpto(int window, InternalChronicleChannel icc, int written, int read) {
+        do {
+            try (DocumentContext dc = icc.readingDocument()) {
+                if (dc.isPresent())
+                    read++;
+            }
+            Jvm.nanoPause();
+        } while (written - read > window);
+        return read;
     }
 }
