7a812f0d5c4fb562a090183c35a4e6de53d63cfa
Peter Lawrey <peter.lawrey@chronicle.software>
2022-08-24 07:45:18 +0100

    Added DummyDataSmallTest

--- src/test/java/net/openhft/chronicle/wire/channel/echo/DummyDataSmallTest.java
+++ src/test/java/net/openhft/chronicle/wire/channel/echo/DummyDataSmallTest.java
@@ -21,26 +21,24 @@ package net.openhft.chronicle.wire.channel.echo;
 import net.openhft.chronicle.bytes.HexDumpBytes;
 import net.openhft.chronicle.wire.BinaryWire;
 import net.openhft.chronicle.wire.Wire;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-public class DummyDataTest {
+public class DummyDataSmallTest {
 
     @Test
     public void readMarshallable() {
-        DummyData dd = new DummyData()
-                .timeNS(12345)
+        DummyDataSmall dd = new DummyDataSmall();
+        dd.timeNS(12345)
                 .data(new byte[16]);
         final HexDumpBytes bytes = new HexDumpBytes();
         Wire wire = new BinaryWire(bytes);
-        wire.getValueOut().object(DummyData.class, dd);
+        wire.getValueOut().object(DummyDataSmall.class, dd);
         assertEquals("" +
-                        "82 1c 00 00 00 39 30 00 00 00 00 00 00 10 00 00 # DummyData\n" +
-                        "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n" +
-                        "00\n",
+                        "80 19 39 30 00 00 00 00 00 00 10 00 00 00 00 00 # DummyDataSmall\n" +
+                        "00 00 00 00 00 00 00 00 00 00 00\n",
                 bytes.toHexString());
         DummyData dd2 = new DummyData();
         wire.getValueIn().object(dd2, DummyData.class);
