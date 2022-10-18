3ffd8d2b9c2c5a3653221858907b61e474c74ddb
Rob Austin <rob.austin@boundedbuffer.co.uk>
2022-10-18 18:03:54 +0100

    moved tests out of internal

---
+++ src/test/java/net/openhft/chronicle/wire/MyClass2.java
@@ -0,0 +1,7 @@
+package net.openhft.chronicle.wire;
+
+public class MyClass2 extends SelfDescribingMarshallable {
+
+    MyClass3 myClass;
+
+}
--- src/test/java/net/openhft/chronicle/wire/internal/MyClass2.java
+++
@@ -1,9 +0,0 @@
-package net.openhft.chronicle.wire.internal;
-
-import net.openhft.chronicle.wire.SelfDescribingMarshallable;
-
-public class MyClass2 extends SelfDescribingMarshallable {
-
-    MyClass3 myClass;
-
-}
