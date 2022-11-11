8e0ed8a25ce213c1112fdaca0de1320f07d07a1e
Peter Lawrey <peter.lawrey@chronicle.software>
2022-11-11 10:33:58 +0000

    Added a simple use of wire with a Person DTO

---
+++ src/test/java/net/openhft/chronicle/wire/examples/PersonOps.java
@@ -0,0 +1,5 @@
+package net.openhft.chronicle.wire.examples;
+
+public interface PersonOps {
+   void addPerson(Person p);
+}
---
+++ src/test/java/net/openhft/chronicle/wire/examples/PersonWireMain.java
@@ -0,0 +1,53 @@
+package net.openhft.chronicle.wire.examples;
+
+import net.openhft.chronicle.bytes.MethodReader;
+import net.openhft.chronicle.core.pool.ClassAliasPool;
+import net.openhft.chronicle.wire.Marshallable;
+import net.openhft.chronicle.wire.Wire;
+import net.openhft.chronicle.wire.converter.Base85;
+
+import static net.openhft.chronicle.core.time.SystemTimeProvider.CLOCK;
+
+public class PersonWireMain {
+    public static void main(String[] args) {
+        // use the short name of the class without needing the package
+        ClassAliasPool.CLASS_ALIASES.addAlias(Person.class);
+
+        Person p1 = new Person()
+                .name("George Ball")
+                .timestampNS(CLOCK.currentTimeNanos())
+                .userName(Base85.INSTANCE.parse("georgeb"));
+        System.out.println("p1: " + p1);
+
+        Wire yWire = Wire.newYamlWireOnHeap();
+        p1.writeMarshallable(yWire);
+        System.out.println(yWire);
+
+        Person p2 = new Person();
+        p2.readMarshallable(yWire);
+        System.out.println("p2: " + p2);
+
+        yWire.clear();
+
+        final PersonOps personOps = yWire.methodWriter(PersonOps.class);
+        personOps.addPerson(
+                Marshallable.fromString(Person.class, "" +
+                        "name: Alice Smith\n" +
+                        "timestampNS: 2022-11-11T10:11:54.7071341\n" +
+                        "userName: alices\n"));
+
+        personOps.addPerson(p1);
+
+        personOps.addPerson(new Person()
+                .name("Bob Singh")
+                .timestampNS(CLOCK.currentTimeNanos())
+                .userName(Base85.INSTANCE.parse("bobs")));
+
+        System.out.println(yWire);
+
+        MethodReader reader = yWire.methodReader(
+                (PersonOps) p -> System.out.println("added " + p));
+        for (int i = 0; i < 3; i++)
+            reader.readOne();
+    }
+}
