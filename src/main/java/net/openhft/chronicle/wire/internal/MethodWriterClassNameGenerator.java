0c15d2eba5ded0cd5e0d51f94629204420522ea6
Rob Austin <rob.austin@boundedbuffer.co.uk>
2023-04-06 14:27:43 +0100

    updated to be the same as develop

--- src/main/java/net/openhft/chronicle/wire/internal/MethodWriterClassNameGenerator.java
+++ src/main/java/net/openhft/chronicle/wire/internal/MethodWriterClassNameGenerator.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package net.openhft.chronicle.wire.internal;
 
 import net.openhft.chronicle.core.Maths;
@@ -15,20 +10,21 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Set;
 
 public class MethodWriterClassNameGenerator {
+
     private static final int MAXIMUM_CLASS_NAME_LENGTH = 255 - ".class".length();
-    private static final LongConverter CLASSNAME_HASH_LONG_CONVERTER;
-    private static final int MAX_LENGTH_OF_HASH;
+    /**
+     * This long converter must produce only characters that are safe to appear in filenames
+     */
+    private static final LongConverter CLASSNAME_HASH_LONG_CONVERTER = Base32LongConverter.INSTANCE;
+    private static final int MAX_LENGTH_OF_HASH = CLASSNAME_HASH_LONG_CONVERTER.asText(Long.MIN_VALUE).length();
 
-    public MethodWriterClassNameGenerator() {
-    }
+    @NotNull
+    public String getClassName(@NotNull Set<Class<?>> interfaces, @Nullable String genericEvent, boolean metaData, boolean intercepting, @NotNull WireType wireType, boolean verboseTypes) {
+        final StringBuilder sb = new StringBuilder();
 
-    public @NotNull String getClassName(@NotNull Set<Class<?>> interfaces, @Nullable String genericEvent, boolean metaData, boolean intercepting, @NotNull WireType wireType, boolean verboseTypes) {
-        StringBuilder sb = new StringBuilder();
-        interfaces.forEach((i) -> {
-            if (i.getEnclosingClass() != null) {
+        interfaces.forEach(i -> {
+            if (i.getEnclosingClass() != null)
                 sb.append(i.getEnclosingClass().getSimpleName());
-            }
-
             sb.append(i.getSimpleName());
         });
         int endOfInterfacesIndex = sb.length();
@@ -41,20 +37,15 @@ public class MethodWriterClassNameGenerator {
         sb.append("MethodWriter");
         if (sb.length() > MAXIMUM_CLASS_NAME_LENGTH) {
             int firstIndexTruncated = endOfInterfacesIndex - (sb.length() - MAXIMUM_CLASS_NAME_LENGTH) - MAX_LENGTH_OF_HASH;
-            long hashOfNonTruncatedClassName = Maths.hash64(sb);
+            final long hashOfNonTruncatedClassName = Maths.hash64(sb);
             sb.delete(firstIndexTruncated, endOfInterfacesIndex);
             sb.insert(firstIndexTruncated, Base32LongConverter.INSTANCE.asText(hashOfNonTruncatedClassName));
         }
-
         return sb.toString();
     }
 
-    private @NotNull String toFirstCapCase(@NotNull String name) {
+    @NotNull
+    private String toFirstCapCase(@NotNull String name) {
         return Character.toUpperCase(name.charAt(0)) + name.substring(1).toLowerCase();
-    }
-
-    static {
-        CLASSNAME_HASH_LONG_CONVERTER = Base32LongConverter.INSTANCE;
-        MAX_LENGTH_OF_HASH = CLASSNAME_HASH_LONG_CONVERTER.asText(Long.MIN_VALUE).length();
     }
 }
