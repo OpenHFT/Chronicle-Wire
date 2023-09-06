270936f82001f8213cada736d9345b530c566351
Kevin Powe <kevin.powe@chronicle.software>
2023-09-06 16:11:57 +1000

    Added throwIllegalArgumentExceptionOnMissingClassForField

--- src/test/java/net/openhft/chronicle/wire/marshallable/IAEOnMissingClassTest.java
+++ src/test/java/net/openhft/chronicle/wire/marshallable/IAEOnMissingClassTest.java
@@ -1,6 +1,10 @@
 package net.openhft.chronicle.wire.marshallable;
 
 import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.core.Jvm;
+import net.openhft.chronicle.core.pool.ClassAliasPool;
+import net.openhft.chronicle.wire.AbstractMarshallableCfg;
+import net.openhft.chronicle.wire.Marshallable;
 import net.openhft.chronicle.wire.TextWire;
 import net.openhft.chronicle.wire.Wire;
 import org.junit.Test;
@@ -16,4 +20,47 @@ public class IAEOnMissingClassTest {
         System.out.println(object);
         assertNotNull(object);
     }
+
+    class TwoFields extends AbstractMarshallableCfg {
+
+        private String name;
+        private Object fieldOne;
+        private Object fieldTwo;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Object getFieldOne() {
+            return fieldOne;
+        }
+
+        public void setFieldOne(Object fieldOne) {
+            this.fieldOne = fieldOne;
+        }
+
+        public Object getFieldTwo() {
+            return fieldTwo;
+        }
+
+        public void setFieldTwo(Object fieldTwo) {
+            this.fieldTwo = fieldTwo;
+        }
+    }
+
+    static {
+        ClassAliasPool.CLASS_ALIASES.addAlias(TwoFields.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwIllegalArgumentExceptionOnMissingClassForField() {
+        String simpleObject = "!TwoFields { name: \"henry\", fieldOne: !ThisClassDoesntExist }";
+        Jvm.startup().on(IAEOnMissingClassTest.class, "Value of illegal.argument.for.missing.class.alias: " + Jvm.getBoolean("illegal.argument.for.missing.class.alias"));
+        final TwoFields simple = Marshallable.fromString(simpleObject);
+    }
+
 }
