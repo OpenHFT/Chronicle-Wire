d7cbb9a173191465d15e24910ae488065b72d325
Tom Dellmann <tom.dellmann@chronicle.software>
2024-02-27 20:32:28 +0000

    Tweak test

--- src/test/java/net/openhft/chronicle/wire/trivial/OrderbookTest.java
+++ src/test/java/net/openhft/chronicle/wire/trivial/OrderbookTest.java
@@ -10,18 +10,19 @@ public class OrderbookTest {
     public void test() {
 
         String expected = "!net.openhft.chronicle.wire.trivial.Orderbook {\n" +
-                "  eventTime: 2023-08-23T11:49:04.308,\n" +
+                "  eventTime: \"2023-08-23T11:49:04.308\",\n" +
                 "  symbol: EUR/USD,\n" +
+                "  exchange: \"\",\n" +
                 "  bid: [\n" +
-                "    { price: 91, qty: 500K },\n" +
-                "    { price: 90, qty: 1M }\n" +
+                "    { price: 91.0, qty: 500.0 },\n" +
+                "    { price: 90.0, qty: 1.0 }\n" +
                 "  ],\n" +
                 "  ask: [\n" +
-                "    { price: 92, qty: 500K },\n" +
-                "    { price: 93, qty: 1M },\n" +
-                "    { price: 93, qty: 1M }\n" +
+                "    { price: 92.0, qty: 500.0 },\n" +
+                "    { price: 93.0, qty: 1.0 },\n" +
+                "    { price: 93.0, qty: 1.0 }\n" +
                 "  ]\n" +
-                "}";
+                "}\n";
         Object o = Marshallable.fromString(expected);
 
         Assert.assertEquals(expected, o.toString());
