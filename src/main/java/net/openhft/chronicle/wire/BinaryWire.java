dbe725bd210a994a0489ef45044eee20dca7485c
Greg Allen <420943+gregallen@users.noreply.github.com>
2024-03-12 15:01:55 +0000

    Optimise length check BinaryWire.text(CharSequence) #848

--- src/main/java/net/openhft/chronicle/wire/BinaryWire.java
+++ src/main/java/net/openhft/chronicle/wire/BinaryWire.java
@@ -1499,8 +1499,8 @@ public class BinaryWire extends AbstractWire implements Wire {
             } else {
                 if (bytes.retainedHexDumpDescription())
                     bytes.writeHexDumpDescription(s);
-                long utflen = AppendableUtil.findUtf8Length(s);
-                if (utflen < 0x20) {
+                long utflen;
+                if (s.length() < 0x20 && (utflen = AppendableUtil.findUtf8Length(s)) < 0x20) {
                     bytes.writeUnsignedByte((int) (STRING_0 + utflen)).appendUtf8(s);
                 } else {
                     writeCode(STRING_ANY);
