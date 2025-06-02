fa3553932a29a9c62815b2a15b60152f46a9546a
Peter Lawrey <peter.lawrey@chronicle.software>
2025-06-02 11:39:01 +0100

    Deep-dive Javadoc sweep & internal comment clean-up (#1173)
    * Improve Wires javadoc (#1150)
    
    * Improve WireType javadoc (#1151)
    
    * Improve WireKey Javadoc (#1152)
    
    * Improve Javadoc for comment and rollback interfaces (#1153)
    
    * Improve javadoc for WireTypeConverter (#1154)
    
    * Improve ReflectionUtil and WireDumper Javadoc (#1155)

--- src/main/java/net/openhft/chronicle/wire/CommentAnnotationNotifier.java
+++ src/main/java/net/openhft/chronicle/wire/CommentAnnotationNotifier.java
@@ -18,16 +18,22 @@
 package net.openhft.chronicle.wire;
 
 /**
- * Represents an interface to notify whether a certain element has a preceding comment annotation.
- * Implementers can use this to handle scenarios where knowledge of preceding comments is required.
+ * <b>INTERNAL USE ONLY.</b> This interface is used by some {@link ValueOut}
+ * implementations (for example for YAML) to signal to
+ * {@link net.openhft.chronicle.wire.WireMarshaller.FieldAccess} that a
+ * {@link Comment} annotation is present on a field. This allows the
+ * {@code FieldAccess} to format the comment using the field's value after the
+ * value itself has been written.
  */
 interface CommentAnnotationNotifier {
 
     /**
-     * Notifies the implementer whether a certain element has a preceding comment annotation.
+     * Notifies the implementer whether a certain element has a preceding comment
+     * annotation.
      *
-     * @param hasCommentAnnotation A flag indicating if the element has a preceding comment.
-     *                             True if it has, otherwise false.
+     * @param hasCommentAnnotation {@code true} if a {@link Comment} annotation is
+     *                             associated with the field about to be written,
+     *                             {@code false} otherwise.
      */
     void hasPrecedingComment(boolean hasCommentAnnotation);
 }
