ad3e90f77e804db10792e228c11bf7bc20736bcb
Peter Lawrey <peter.lawrey@chronicle.software>
2025-06-02 10:51:50 +0100

    Improve javadoc for MessagePathClassifier and RecordHistory

--- src/main/java/net/openhft/chronicle/wire/utils/RecordHistory.java
+++ src/main/java/net/openhft/chronicle/wire/utils/RecordHistory.java
@@ -1,10 +1,19 @@
 package net.openhft.chronicle.wire.utils;
 
+import net.openhft.chronicle.wire.VanillaMessageHistory;
+
 /**
- * Write a history before each message
+ * Functional interface for publishers that prepend a {@link VanillaMessageHistory}
+ * before the next message.
  *
- * @param <T> interface of messages with prepended history
+ * @param <T> the type of the publishing interface augmented with history recording
  */
 public interface RecordHistory<T> {
-    T history(net.openhft.chronicle.wire.VanillaMessageHistory history);
+    /**
+     * Records the supplied history so that it is written before the next message.
+     *
+     * @param history the history to prepend
+     * @return typically {@code this} to allow method chaining
+     */
+    T history(VanillaMessageHistory history);
 }
