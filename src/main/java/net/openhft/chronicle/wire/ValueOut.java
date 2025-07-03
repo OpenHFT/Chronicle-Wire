c04c02ae2ca14173179e09b1b270441dfb3ccbb2
Peter Lawrey <peter.lawrey@chronicle.software>
2025-07-03 16:32:43 +0100

    Move MapMarshallable to internal

--- src/main/java/net/openhft/chronicle/wire/ValueOut.java
+++ src/main/java/net/openhft/chronicle/wire/ValueOut.java
@@ -31,6 +31,7 @@ import net.openhft.chronicle.core.util.CoreDynamicEnum;
 import net.openhft.chronicle.core.util.ObjectUtils;
 import net.openhft.chronicle.core.values.*;
 import net.openhft.chronicle.threads.NamedThreadFactory;
+import net.openhft.chronicle.wire.internal.MapMarshaller;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -1804,45 +1805,4 @@ public interface ValueOut {
         // Do nothing in the default implementation
     }
 
-    /**
-     * MapMarshaller is a utility for serializing a Map into a Wire format.
-     * This is an inner class used for handling the custom marshalling process for Map objects.
-     * Its primary function is to loop through a Map's entries and write each key-value pair to the Wire.
-     */
-    class MapMarshaller<K, V> implements WriteMarshallable {
-        private Map<K, V> map;
-        private Class<K> kClass;
-        private Class<V> vClass;
-        private boolean leaf;
-
-        /**
-         * Configures the MapMarshaller with the provided parameters.
-         *
-         * @param map The map to be marshalled.
-         * @param kClass The class type of the map's key.
-         * @param vClass The class type of the map's value.
-         * @param leaf A flag indicating if the current node is a leaf in a structure.
-         */
-        void params(@Nullable Map<K, V> map, @NotNull Class<K> kClass, @NotNull Class<V> vClass, boolean leaf) {
-            this.map = map;
-            this.kClass = kClass;
-            this.vClass = vClass;
-            this.leaf = leaf;
-        }
-
-        /**
-         * Converts and writes the Map's entries to the Wire format.
-         *
-         * @param wire The WireOut instance to write to.
-         */
-        @Override
-        public void writeMarshallable(@NotNull WireOut wire) throws InvalidMarshallableException {
-            for (@NotNull Map.Entry<K, V> entry : map.entrySet()) {
-                ValueOut valueOut = wire.writeEvent(kClass, entry.getKey());
-                boolean wasLeaf = valueOut.swapLeaf(leaf);
-                valueOut.object(vClass, entry.getValue());
-                valueOut.swapLeaf(wasLeaf);
-            }
-        }
-    }
 }
