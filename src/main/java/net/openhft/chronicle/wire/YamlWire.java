2e10651dc296c62f22e696466528db3ea2623f64
Peter Lawrey <peter.lawrey@chronicle.software>
2025-10-15 12:15:18 +0100

    Adv/01 vers2 (#1204)
    * fix: update copyright year to 2025 in multiple files
    
    * docs: enable section numbering and improve formatting across multiple documents
    
    * fix: update copyright year to 2025 in multiple files
    
    * fix: update copyright year to 2025 in multiple files
    
    * fix: update copyright year to 2025 in multiple files
    
    * fix: update copyright year to 2025 in multiple files
    
    * docs: improve clarity and formatting in AGENTS.md
    
    * fix: update copyright year to 2025 in jlbh-test.sh and add LICENSE.adoc
    
    * fix: update copyright year to 2025 in pom.xml
    
    * fix: update copyright year to 2025 in pom.xml
    
    * fix: update copyright year to 2025 in system.properties
    
    * docs: enhance system properties documentation with detailed descriptions and usage notes
    
    * revert removal of anchors
    
    * revert removal of anchors
    
    * docs: update mermaid syntax for GitHub compatibility in architecture documentation
    
    * docs: fix mermaid syntax for proper rendering in GitHub
    
    * docs: fix mermaid syntax for proper rendering in GitHub
    
    * docs: update mermaid syntax for HTML compatibility in EventsByMethod documentation
    
    * docs: correct Java code example in EventsByMethod documentation

--- src/main/java/net/openhft/chronicle/wire/YamlWire.java
+++ src/main/java/net/openhft/chronicle/wire/YamlWire.java
@@ -549,8 +549,6 @@ public class YamlWire extends YamlWireOut<YamlWire> {
     private void copyOne(WireOut wire, boolean nested) throws InvalidMarshallableException {
         ValueOut wireValueOut = wire.getValueOut();
         switch (yt.current()) {
-            case NONE:
-                break;
             case COMMENT:
                 wire.writeComment(yt.text());
                 break;
@@ -564,10 +562,6 @@ public class YamlWire extends YamlWireOut<YamlWire> {
                 yt.next();
                 copyOne(wire, true);
                 valueOut2.endTypePrefix();
-                break;
-            case DIRECTIVE:
-                break;
-            case DOCUMENT_END:
                 break;
             case DIRECTIVES_END:
                 yt.next();
@@ -593,10 +587,6 @@ public class YamlWire extends YamlWireOut<YamlWire> {
                 }
                 break;
             }
-            case SEQUENCE_END:
-                break;
-            case SEQUENCE_ENTRY:
-                break;
             case SEQUENCE_START: {
                 yt.next();
                 YamlWire yw = this;
@@ -618,15 +608,17 @@ public class YamlWire extends YamlWireOut<YamlWire> {
             case LITERAL:
                 wireValueOut.text(yt.text());
                 break;
+            case NONE:
+            case DIRECTIVE:
+            case DOCUMENT_END:
+            case SEQUENCE_END:
+            case SEQUENCE_ENTRY:
             case ANCHOR:
-                break;
             case ALIAS:
-                break;
             case RESERVED:
-                break;
             case STREAM_END:
-                break;
             case STREAM_START:
+            default:
                 break;
         }
     }
