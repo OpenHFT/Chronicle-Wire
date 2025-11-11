12d694ddaaeb1cf79528fc34b226da28706feab0
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-11 15:07:48 +0000

    Update copyright headers to reflect 2013-2025 and SPDX-License-Identifier: Apache-2.0

--- src/main/java/net/openhft/chronicle/wire/internal/package-info.java
+++ src/main/java/net/openhft/chronicle/wire/internal/package-info.java
@@ -1,22 +1,4 @@
 /*
- * Copyright 2016-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
- */
-/**
- * Provides internal utility classes and implementation details for the Chronicle Wire library.
- * <p>
- * Classes here are not part of the public API and may change without notice. They underpin
- * wire implementations, marshalling and code generation.
- *
- * <p>
- *  Specifically, the following actions (including, but not limited to) are not allowed
- *  on internal classes and packages:
- *  <ul>
- *      <li>Casting to</li>
- *      <li>Reflection of any kind</li>
- *      <li>Explicit Serialize/deserialize</li>
- *  </ul>
- * <p>
- * @see net.openhft.chronicle.wire.Wire
- * @see net.openhft.chronicle.wire.Marshallable
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
  */
 package net.openhft.chronicle.wire.internal;
