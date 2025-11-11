e4b7df7e49bc35e3230dacc470bbbf71344b154d
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-11 16:44:05 +0000

    Update test descriptions and adjust Jacoco coverage thresholds

--- src/test/java/net/openhft/chronicle/wire/IgnoreHighOrderBitsTest.java
+++ src/test/java/net/openhft/chronicle/wire/IgnoreHighOrderBitsTest.java
@@ -16,7 +16,7 @@ public class IgnoreHighOrderBitsTest extends WireTestCommon {
 
     /**
      * Writes to the output stream the eight low-order bits of the argument b. The 24 high-order bits of b are ignored.
-     * see https://docs.oracle.com/javase/7/docs/api/java/io/DataOutput.html#write(int)
+     * see https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/io/DataOutput.html#write(int)
      */
     @Test
     public void testWriteByte() throws IOException {
